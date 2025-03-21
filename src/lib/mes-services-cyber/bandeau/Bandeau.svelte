e8fa4747937a0e46c0c58205bd9815ff5d97ad9e
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2025-03-21 10:40:31 +0100

    [COMPOSANT] Décline <MesServicesCyberBandeau> en tablette et desktop

--- src/lib/mes-services-cyber/bandeau/Bandeau.svelte
+++ src/lib/mes-services-cyber/bandeau/Bandeau.svelte
@@ -12,11 +12,12 @@
 </script>
 
 {#if affichage === "visible"}
-  <div class="contenu" transition:slide={{ duration: 160 }}>
+  <div class="largeur-totale">
     <img src={tuile} alt="" class="tuiles-background" />
-    <h4>
-      🚀 L'ANSSI lance MesServicesCyber !
+
+    <div class="contenu" transition:slide={{ duration: 160 }}>
       <button
+        class="fermer"
         on:click={() => {
           affichage = "invisible";
           localStorage.setItem(CLE_LOCAL_STORAGE, "invisible");
@@ -24,24 +25,26 @@
       >
         <img src={croix} alt="fermer" />
       </button>
-    </h4>
-    <p>Avec MesServicesCyber découvrez les ressources cyber de l'ANSSI et ses partenaires.</p>
 
-    <a href="https://messervices.cyber.gouv.fr/" target="_blank" rel="noopener">
-      Je découvre MesServicesCyber
-    </a>
+      <div class="textes">
+        <h4>
+          🚀 Découvrez MesServicesCyber ! <span class="badge visible-tablette">nouveauté</span>
+        </h4>
+        <p>
+          MesServicesCyber, la plateforme pour faciliter l'accès de aux services et ressources de
+          l'ANSSI et de ses partenaires.
+        </p>
+      </div>
+
+      <a href="https://messervices.cyber.gouv.fr/" target="_blank" rel="noopener">
+        Je découvre MesServicesCyber
+      </a>
+    </div>
   </div>
 {/if}
 
 <style lang="scss">
-  .contenu {
-    color: white;
-    font-family: "Marianne";
-    padding: 16px;
-
-    font-size: 0.875rem;
-    line-height: 1.5rem;
-
+  .largeur-totale {
     position: relative;
     overflow: hidden;
     background: #0d0c21;
@@ -50,55 +53,107 @@
       position: absolute;
       top: 0;
       left: 0;
-      transform: scale(60%);
-      transform-origin: 0 0;
       pointer-events: none;
     }
 
-    h4 {
+    .contenu {
+      color: white;
+      font-family: "Marianne";
+      padding: 16px;
+      max-width: 1200px;
       font-size: 0.875rem;
       line-height: 1.5rem;
-      font-weight: 700;
-      margin: 0;
-
       display: flex;
-      flex-direction: row;
-      justify-content: space-between;
+      flex-direction: column;
 
-      button {
+      .fermer {
         border: none;
         cursor: pointer;
         background: none;
         padding: 0;
         transition: transform 0.2s ease-in-out;
+        position: absolute;
+        top: 20px;
+        right: 20px;
 
         &:hover {
           transform: scale(1.3);
         }
       }
-    }
 
-    p {
-      font-weight: 400;
-      font-size: 0.875rem;
-      line-height: 1.5rem;
-      margin: 0;
-      padding-right: 32px;
-    }
+      .textes {
+        display: flex;
+        flex-direction: column;
+        @include a-partir-de(tablette) {
+          gap: 8px;
+        }
+      }
 
-    a {
-      display: block;
-      color: #242424;
-      background: white;
-      margin-top: 8px;
-      text-align: center;
-      font-size: 0.875rem;
-      line-height: 1.5rem;
-      text-decoration: none;
+      h4 {
+        font-size: 0.875rem;
+        line-height: 1.5rem;
+        font-weight: 700;
+        margin: 0;
 
-      gap: 8px;
-      border-radius: 4px;
-      padding: 4px 12px;
+        display: flex;
+        flex-direction: row;
+        align-items: baseline;
+        gap: 8px;
+
+        @include a-partir-de(desktop) {
+          font-size: 1rem;
+          line-height: 1.5rem;
+        }
+
+        .badge {
+          color: #19753c;
+          background: #b8fec9;
+          border-radius: 4px;
+          padding-right: 6px;
+          padding-left: 6px;
+          font-size: 0.75rem;
+          font-weight: 700;
+          line-height: 1.25rem;
+          text-transform: uppercase;
+          transform: translateY(-3px);
+        }
+      }
+
+      p {
+        font-weight: 400;
+        font-size: 0.875rem;
+        line-height: 1.5rem;
+        margin: 0;
+        padding-right: 32px;
+
+        @include a-partir-de(desktop) {
+          font-size: 1rem;
+          line-height: 1.5rem;
+        }
+      }
+
+      a {
+        display: block;
+        color: #242424;
+        background: white;
+        margin: 8px auto 0 0;
+        text-align: center;
+        font-size: 0.875rem;
+        line-height: 1.5rem;
+        text-decoration: none;
+        gap: 8px;
+        border-radius: 4px;
+        padding: 4px 12px;
+        white-space: nowrap;
+        height: fit-content;
+        width: 100%;
+        max-width: 312px;
+        box-sizing: border-box;
+
+        @include a-partir-de(tablette) {
+          max-width: 248px;
+        }
+      }
     }
   }
 </style>
