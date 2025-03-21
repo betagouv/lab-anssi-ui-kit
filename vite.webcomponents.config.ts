a635dbbb371e9febcfbacb950b257cfa13d6d997
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2025-03-21 13:19:13 +0100

    [SOIN] Prettier

--- vite.webcomponents.config.ts
+++ vite.webcomponents.config.ts
@@ -1,20 +1,20 @@
-import { svelte } from '@sveltejs/vite-plugin-svelte';
-import { defineConfig } from 'vite';
-import { resolve } from 'path';
-import genereJSX from './outils/vite-plugin-genere-jsx.js';
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import genereJSX from "./outils/vite-plugin-genere-jsx.js";
 
 // Ce fichier permet de build la librairie en mode "WebComponents"
 // En suivant cette issue : https://github.com/sveltejs/kit/issues/10320
 
 export default defineConfig({
-	build: {
-		lib: {
-			entry: resolve(__dirname, 'dist/index.js'),
-			name: 'WebComponents',
-			fileName: 'index',
-			formats: ['iife']
-		},
-		outDir: 'dist/webcomponents'
-	},
-	plugins: [svelte(), genereJSX()]
+  build: {
+    lib: {
+      entry: resolve(__dirname, "dist/index.js"),
+      name: "WebComponents",
+      fileName: "index",
+      formats: ["iife"],
+    },
+    outDir: "dist/webcomponents",
+  },
+  plugins: [svelte(), genereJSX()],
 });
