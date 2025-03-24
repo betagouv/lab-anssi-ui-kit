2fcd3ab7b835e61063383781d2f60cd4e4d313a1
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2025-03-24 20:37:35 +0100

    Ajoute un outillage permettant d'injecter des `nonce` dans les balise de style
    ... Les webcomposants nous posent des problèmes de CSP, car ils injectent eux même des balises `<style>` dans le shadow DOM. Grâce à ce plugin, on ajoute un `nonce` à ces balises.
    
    Ce nonce doit être passé lors de l'import du script via `data-nonce`.

---
+++ outils/vite-plugin-injecte-nonce.ts
@@ -0,0 +1,25 @@
+import type { Plugin } from "vite";
+
+export default function injecteNonce(): Plugin {
+  return {
+    name: "injecte-nonce",
+    enforce: "post",
+    generateBundle(_options, bundle) {
+      console.log("📝 Ajout de la gestion du Nonce");
+
+      for (const file of Object.values(bundle)) {
+        if (file.type === "chunk" && file.code) {
+          file.code = `const nonce = document.currentScript?.dataset?.nonce;\n${file.code}`;
+          // Remplace `const a = u("style");`
+          // par `const a = u("style");a.nonce=nonce;`
+          file.code = file.code.replace(
+            /const (.)\s*=\s*.\("style"\);/gm,
+            (match, nomVariable) => `${match}${nomVariable}.nonce=nonce;`,
+          );
+        }
+      }
+
+      console.log("✅");
+    },
+  };
+}
