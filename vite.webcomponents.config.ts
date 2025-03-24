6204926ade8bb28a49c9653431523769ff5077a2
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2025-03-24 20:37:35 +0100

    Ajoute un outillage permettant d'injecter des `nonce` dans les balise de style
    ... Les webcomposants nous posent des problèmes de CSP, car ils injectent eux même des balises `<style>` dans le shadow DOM. Grâce à ce plugin, on ajoute un `nonce` à ces balises.
    
    Ce nonce doit être passé lors de l'import du script via `data-nonce`.

--- vite.webcomponents.config.ts
+++ vite.webcomponents.config.ts
@@ -1,7 +1,8 @@
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { defineConfig } from "vite";
 import { resolve } from "path";
-import genereJSX from "./outils/vite-plugin-genere-jsx.js";
+import genereJSX from "./outils/vite-plugin-genere-jsx";
+import injecteNonce from "./outils/vite-plugin-injecte-nonce";
 
 // Ce fichier permet de build la librairie en mode "WebComponents"
 // En suivant cette issue : https://github.com/sveltejs/kit/issues/10320
@@ -16,5 +17,5 @@ export default defineConfig({
     },
     outDir: "dist/webcomponents",
   },
-  plugins: [svelte(), genereJSX()],
+  plugins: [svelte(), genereJSX(), injecteNonce()],
 });
