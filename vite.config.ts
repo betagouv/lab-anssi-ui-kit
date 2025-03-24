055292cca3076369f299821b9910f4679094cd09
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2025-03-25 00:49:35 +0100

    Ajoute la possibilité d'utiliser la variable d'environnement `VITE_LAB_ANSSI_UI_KIT_ASSET_BASE` dans le code SCSS
    ... permettant ainsi de définir des `background-url`, des `:before` ou autre qui pointeront vers le CDN du Lab dans le bundle buildé de l'UI kit.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
 import { sveltekit } from "@sveltejs/kit/vite";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
+
+// On ne build jamais pour le local : allons chercher en dur les variables de PROD.
+const varEnvDeProduction = loadEnv("production", process.cwd(), "VITE_");
 
 export default defineConfig({
   plugins: [sveltekit()],
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: "@use 'src/variables.scss' as *; @use 'src/responsive' as *;",
+        additionalData: `
+          @use 'src/variables.scss' as *; 
+          @use 'src/responsive.scss' as *; 
+          @use 'src/assets.scss' as *;
+          $assets-url-base: '${varEnvDeProduction.VITE_LAB_ANSSI_UI_KIT_ASSET_BASE}'; 
+        `,
       },
     },
   },
