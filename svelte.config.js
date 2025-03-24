3da2e9bf805c5cdc9f93cf822a0110a166b6ab74
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2025-03-24 11:50:38 +0100

    [SOIN] Prettier

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,11 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from "@sveltejs/adapter-auto";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	compilerOptions: {
-		customElement: true
-	},
-	preprocess: vitePreprocess(),
-
-	kit: {
-		adapter: adapter()
-	}
+  compilerOptions: { customElement: true },
+  preprocess: vitePreprocess(),
+  kit: { adapter: adapter() },
 };
 
 export default config;
