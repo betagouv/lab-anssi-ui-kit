035356fc6e6e365de5a9e030b0e1bc15eabd1558
Alexis Brones <alexis.brones@beta.gouv.fr>
2025-03-26 14:29:59 +0100

    Ajoute le bouton en-tête redirigeant vers MSC  Please enter the commit message for your changes. Lines starting

---
+++ src/lib/suite-cyber/navigation/AccueilMSC.svelte
@@ -0,0 +1,73 @@
+<svelte:options customElement="lab-anssi-bouton-suite-cyber-navigation-accueil-msc" />
+
+<script lang="ts">
+  import { srcAsset } from "$lib/mes-services-cyber/assets.js";
+  const accueilMSC = srcAsset("/icones/bouton-accueil-msc.svg");
+  const chevronDroite = srcAsset("/icones/chevron-noir.svg");
+</script>
+
+<a
+  href="https://messervices.cyber.gouv.fr"
+  target="_blank"
+  class="accueil-msc"
+  id="mes-services-cyber"
+>
+  <div class="icone">
+    <img src={accueilMSC} alt="MesServicesCyber" />
+  </div>
+  <label for="mes-services-cyber">
+    MesServicesCyber
+    <span>Gérer mon profil et mes services</span>
+  </label>
+  <div class="chevron">
+    <img src={chevronDroite} alt="Aller sur MesServicesCyber" />
+  </div>
+</a>
+
+<style lang="scss">
+  .accueil-msc {
+    font-family: Marianne;
+    font-style: normal;
+    font-weight: normal;
+    cursor: pointer;
+    display: flex;
+    margin-top: 8px;
+    padding: 12px 16px;
+    align-items: center;
+    gap: 12px;
+    align-self: stretch;
+    text-decoration: none;
+
+    &:hover {
+      background-color: #f6f6f6;
+    }
+    &:active {
+      background-color: #ededed;
+    }
+
+    label {
+      display: flex;
+      padding-bottom: 4px;
+      flex-direction: column;
+      justify-content: center;
+      align-items: flex-start;
+      flex: 1 0 0;
+      color: #161616;
+      font-family: Marianne;
+      font-size: 18px;
+      font-style: normal;
+      font-weight: 700;
+      line-height: 28px;
+      cursor: pointer;
+
+      span {
+        color: #3a3a3a;
+        font-family: Marianne;
+        font-size: 12px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: 20px;
+      }
+    }
+  }
+</style>
