f1ee243fb451a67e2651600d1316fcf79f0f8a1a
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2025-03-31 16:37:31 +0200

    Fais remonter un évènement lors du clic sur un lien du centre d'aide

--- stories/CentreAide.story.svelte
+++ stories/CentreAide.story.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
   import type { Hst } from "@histoire/plugin-svelte";
+  import { logEvent } from 'histoire/client';
+
   import CentreAide from "$lib/CentreAide.svelte";
   import OutilSelecteurTheme from "./OutilSelecteurTheme.svelte";
 
@@ -15,13 +17,24 @@
       texte: "🙌 Consulter la F.A.Q.",
       href: "https://aide.messervices.cyber.gouv.fr/fr",
     },
+    {
+      texte: "🎬 Faire une action 'custom'",
+      preventDefault: true,
+      id: "action-custom"
+    },
   ];
   $: chaineLiens = JSON.stringify(liens);
+
+  const gereClicLien = (e: CustomEvent<{target: EventTarget & HTMLAnchorElement}>) => {
+    logEvent('lienclique', e.detail);
+    if(e.detail.target.id === "action-custom")
+      alert("Je fais une action 'custom'")
+  };
 </script>
 
 <Hst.Story title="Composants/Centre d'aide" icon="material-symbols:live-help">
   <OutilSelecteurTheme />
-  <CentreAide {nomService} liens={chaineLiens} />
+  <CentreAide {nomService} liens={chaineLiens} on:lienclique={gereClicLien}/>
 
   <svelte:fragment slot="controls">
     <Hst.Text title="Nom du service" bind:value={nomService} />
