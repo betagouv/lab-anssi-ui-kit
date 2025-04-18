a9b14bb80238b1df365aa14950e25f6c56ea300f
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2025-04-18 17:04:56 +0200

    Corrige la couleur et l'alignement de l'icône « lien externe »

--- stories/mes-services-cyber/lien/LienDiagnosticCyber.story.svelte
+++ stories/mes-services-cyber/lien/LienDiagnosticCyber.story.svelte
@@ -1,17 +1,17 @@
 <script lang="ts">
-  import { LienDiagnosticCyber } from "$lib";
   import type { Hst } from "@histoire/plugin-svelte";
+  import { LienDiagnosticCyber } from "../../../src/lib";
 
   export let Hst: Hst;
+  let versExterne = true;
 </script>
 
 <Hst.Story title="Composants/MesServicesCyber/LienDiagnosticCyber">
   <div style="height: 600px; padding: 30px">
     <div style="margin-left: 300px">
       <h3>Avant</h3>
-      <LienDiagnosticCyber />
+      <LienDiagnosticCyber {versExterne} lien="https://" />
       <h3>Après</h3>
     </div>
-
   </div>
 </Hst.Story>
