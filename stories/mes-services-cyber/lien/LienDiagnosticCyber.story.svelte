d31ef0573f67afe9277767027b100104b2204583
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2025-04-18 10:40:06 +0200

    Supprime un contrôle inutile
    … qui est le résultat d'un copier/coller depuis une autre histoire.

--- stories/mes-services-cyber/lien/LienDiagnosticCyber.story.svelte
+++ stories/mes-services-cyber/lien/LienDiagnosticCyber.story.svelte
@@ -9,16 +9,4 @@
   <div style="height: 600px; padding: 30px">
     <LienDiagnosticCyber />
   </div>
-
-  <svelte:fragment slot="controls">
-    <button
-      style="margin: 30px 0 0 10px; padding: 5px; cursor: pointer;"
-      on:click={() => {
-        localStorage.setItem("lab-anssi-ui-kit-msc-bandeau-affichage", "visible");
-        location.reload();
-      }}
-    >
-      Faire réapparaître
-    </button>
-  </svelte:fragment>
 </Hst.Story>
