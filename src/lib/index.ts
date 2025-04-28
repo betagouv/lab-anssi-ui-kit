5507bfa3a16ea3a3eae9b7de7f977a7fc52aacc0
Bertrand Bougon <bertrand.bougon@gmail.com>
2025-04-28 10:38:45 +0200

    [CORRECTION] Corrige la disponibilité du résumé PSSI
    - Le résumé PSSI n’est plus exporté depuis la version 1.5.0 de l’UI Kit

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,4 +3,4 @@ export { default as MesServicesCyberBandeau } from "./mes-services-cyber/bandeau
 export { default as LienDiagnosticCyber } from "./mes-services-cyber/lien/LienDiagnosticCyber.svelte";
 export { default as NavigationPiedDePage } from "./pied-de-page/NavigationPiedDePage.svelte";
 export { default as SuiteCyberNavigation } from "./suite-cyber/navigation/Navigation.svelte";
-
+export { default as ResumePssi } from "./lab/ResumePssi.svelte";
