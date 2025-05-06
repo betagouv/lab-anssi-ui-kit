b9662de28030a5b3de1fffccc769cd3de506c851
Marc ETOURNEAU <marc.etourneau@beta.gouv.fr>
2025-05-06 12:20:38 +0200

    Créé un composant réutilisable Brique pour les pages vitrines

---
+++ src/lib/lab/vitrines-produits/briques/Brique.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  export let variation: 'primaire' = 'primaire';
+</script>
+
+<section class={variation}>
+  <div class="contenu-brique">
+    <slot />
+  </div>
+</section>
+
+<style lang="scss">
+  section {
+    font-family: Marianne;
+    padding: 48px 16px;
+    text-align: left;
+    width: calc(100vw - 32px);
+    color: white;
+
+    @include a-partir-de(tablette) {
+      padding: 72px 24px;
+      width: calc(100vw - 48px);
+    }
+
+    @include a-partir-de(desktop) {
+      padding: 48px 24px;
+      width: calc(100vw - 48px);
+    }
+
+    &.primaire {
+      background: $brique-background-primaire;
+    }
+  }
+
+  .contenu-brique {
+    max-width: 1200px;
+    margin: 0 auto;
+  }
+</style>
