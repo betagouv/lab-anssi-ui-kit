cd319774b52ed19dee2761a2ec56a23667d35b46
Marc ETOURNEAU <marc.etourneau@beta.gouv.fr>
2025-05-07 09:54:41 +0200

    Ajoute une prévisualisation de la landing page MAC dans Histoire

---
+++ stories/lab/vitrines-produits/LandingMAC.story.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import type { Hst } from "@histoire/plugin-svelte";
+  import BriqueHero from "$lib/lab/vitrines-produits/briques/BriqueHero.svelte";
+  import OutilSelecteurTheme from "../../OutilSelecteurTheme.svelte";
+  import type { Actions, Image } from "$lib/types";
+
+  export let Hst: Hst;
+
+  const actionsDisponibles: Actions = [
+    {
+      titre: 'Devenir Aidant cyber',
+      lien: "",
+      variation: 'primaire'
+    },
+    {
+      titre: "Bénéficier d'un diagnostic cyber",
+      lien: "",
+      variation: 'secondaire'
+    }
+  ]
+
+  const imageAffichee: Image = {
+    lien: 'src/lib/assets/illustrations/aidant-aidee.svg',
+    alt: "Un Aidant en train de converser avec une entité souhaitant bénéficier d'un diagnostic cyber"
+  }
+
+
+</script>
+
+<Hst.Story title="Composants/Lab/Sites vitrines/Landing MonAideCyber" icon="material-symbols:brick-outline">
+  <OutilSelecteurTheme themeSelectionne="MonAideCyber" />
+  <BriqueHero titre="Mon​Aide​Cyber"
+              sousTitre="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id sodales sapien, sit amet ultrices est."
+              illustration={JSON.stringify(imageAffichee)}
+              actions={JSON.stringify(actionsDisponibles)}
+  />
+</Hst.Story>
