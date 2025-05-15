717560201cf04a0202a1231c19da4d77aa38ea9a
Alexis Brones <alexis.brones@beta.gouv.fr>
2025-05-15 11:46:46 +0200

    Ajoute le composant Alerte en mode information.

---
+++ src/lib/Alerte.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+  export let description: string;
+  let estOuvert = true;
+</script>
+
+{#if estOuvert}
+  <div class="alerte">
+    <span class="icone"></span>
+    <div class="contenu">{description}</div>
+    <button
+      class="fermer"
+      on:click={() => {
+        estOuvert = false;
+      }}
+    ></button>
+  </div>
+{/if}
+
+<style lang="scss">
+  .alerte {
+    display: flex;
+    border: 1px solid #0163cb;
+  }
+  .contenu {
+    flex: 1;
+    padding: 8px;
+    color: $texte-secondaire;
+  }
+  .icone {
+    width: 24px;
+    padding: 8px;
+    background: no-repeat center 8px #0163cb url-asset("/icones/information.svg");
+  }
+  .fermer {
+    width: 16px;
+    height: 16px;
+    padding: 8px;
+    margin-top: 4px;
+    margin-right: 4px;
+    background: no-repeat center url-asset("/icones/croix-bleu.svg");
+    border: 0;
+    cursor: pointer;
+  }
+</style>
---
+++ stories/Alerte.story.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import Alerte from "$lib/Alerte.svelte";
+  import type { Hst } from "@histoire/plugin-svelte";
+
+  export let Hst: Hst;
+</script>
+
+<Hst.Story title="Composants/Alerte" icon="material-symbols:live-help">
+  <div style="background-color:white; padding:48px;">
+    <h3>Information</h3>
+    <div style="margin: 16px 0;">
+      <Alerte description="Lorem ipsum dolor sit" />
+    </div>
+    <div style="margin: 16px 0;">
+      <Alerte
+        description="
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam imperdiet laoreet tortor id tristique. Proin vestibulum mi non dui luctus, vitae ornare augue hendrerit."
+      />
+    </div>
+  </div>
+</Hst.Story>
