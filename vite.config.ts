500652c3343f429c66130b45f14ae52214995262
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2025-05-19 19:32:24 +0200

    [CORRECTION] Utilise le bon fichier `.env` en fonction du mode pour `Histoire`

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { loadEnv } from "vite";
 import { defineConfig } from "vitest/config";
+import dotenv from 'dotenv';
+import { resolve } from 'path'
 
-// On ne build jamais pour le local : allons chercher en dur les variables de PROD.
-const varEnvDeProduction = loadEnv("production", process.cwd(), "VITE_");
+// Charge le bon environnement pour faire fonctionner la méthode SCSS `url-asset`
+// - Build webcomponent : "production"
+// - Storybook local : "development"
+// - Storybook déployé : "production"
+const varEnv = loadEnv(process.env.HISTOIRE_ENV ?? "production", process.cwd(), "VITE_");
+
+// On charge manuellement les valeurs d'environnement de la production, car pour une raison qu'on ignore
+// c'est la seule façon de charger Histoire avec nos valeurs
+// Cela permet de faire fonctionner la méthode JS `srcAsset` dans le cas du Storybook déployé
+if(process.env.HISTOIRE_ENV === 'production')
+  dotenv.config({ path: resolve(process.cwd(), '.env.production') })
 
 export default defineConfig({
   plugins: [sveltekit()],
@@ -18,7 +29,7 @@ export default defineConfig({
           @use 'src/variables.scss' as *; 
           @use 'src/responsive.scss' as *; 
           @use 'src/assets.scss' as *;
-          $assets-url-base: '${varEnvDeProduction.VITE_LAB_ANSSI_UI_KIT_ASSET_BASE}'; 
+          $assets-url-base: '${varEnv.VITE_LAB_ANSSI_UI_KIT_ASSET_BASE}'; 
         `,
       },
     },
