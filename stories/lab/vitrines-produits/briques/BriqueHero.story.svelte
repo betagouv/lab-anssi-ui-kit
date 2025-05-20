a6b42168195edb78b272cbd583970dd70bc7606e
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2025-05-20 12:03:58 +0200

    Remplaces les "variantes" de la brique Hero par des contrôles d'Histoire
    ... afin de nettoyer la story et rendre dynamique le rendu du composant

--- stories/lab/vitrines-produits/briques/BriqueHero.story.svelte
+++ stories/lab/vitrines-produits/briques/BriqueHero.story.svelte
@@ -2,43 +2,30 @@
   import type { Hst } from "@histoire/plugin-svelte";
   import BriqueHero from "$lib/lab/vitrines-produits/briques/BriqueHero.svelte";
   import OutilSelecteurTheme from "../../../OutilSelecteurTheme.svelte";
-  import { proprietesDeVariantsHero } from "../donneesProduits";
+  import { genereImageDePlaceholder } from "$lib/generateurImagesPlaceholders";
 
   export let Hst: Hst;
+
+  let badge = true;
+  let titre = "Mon​Service​Sécurisé";
+  let soustitre = "L'outil pour piloter en équipe la sécurité de tous vos services numériques et les homologuer rapidement";
+  let illustration = { lien: genereImageDePlaceholder(600, 400), alt: "Logo placeholder" };
+  let actiongauche = { titre: "Commencer à sécuriser", lien: "" };
+  let actiondroite = { titre: "Être accompagné", lien: "" };
+  let partenaires = [{ lien: "src/lib/assets/illustrations/cnil.svg", alt: "Logo de la CNIL" }];
 </script>
 
 <Hst.Story title="Composants/Lab/Sites vitrines/Brique Hero" icon="material-symbols:brick-outline">
-  <Hst.Variant title="MSS">
-    <OutilSelecteurTheme themeSelectionne="MonServiceSécurisé" />
-    <BriqueHero titre={proprietesDeVariantsHero['mss'].titre}
-                badge={proprietesDeVariantsHero['mss'].badge}
-                soustitre={proprietesDeVariantsHero['mss'].soustitre}
-                illustration={proprietesDeVariantsHero['mss'].illustration}
-                actiongauche={proprietesDeVariantsHero['mss'].actiongauche}
-                actiondroite={proprietesDeVariantsHero['mss'].actiondroite}
-                partenaires={proprietesDeVariantsHero['mss'].partenaires}
-    />
-  </Hst.Variant>
-  <Hst.Variant title="MAC">
-    <OutilSelecteurTheme themeSelectionne="MonAideCyber" />
-    <BriqueHero titre={proprietesDeVariantsHero['mac'].titre}
-                badge={proprietesDeVariantsHero['mac'].badge}
-                soustitre={proprietesDeVariantsHero['mac'].soustitre}
-                illustration={proprietesDeVariantsHero['mac'].illustration}
-                actiongauche={proprietesDeVariantsHero['mac'].actiongauche}
-                actiondroite={proprietesDeVariantsHero['mac'].actiondroite}
-                partenaires={proprietesDeVariantsHero['mac'].partenaires}
-    />
-  </Hst.Variant>
-  <Hst.Variant title="MSC">
-    <OutilSelecteurTheme themeSelectionne="MesServicesCyber" />
-    <BriqueHero titre={proprietesDeVariantsHero['msc'].titre}
-                badge={proprietesDeVariantsHero['msc'].badge}
-                soustitre={proprietesDeVariantsHero['msc'].soustitre}
-                illustration={proprietesDeVariantsHero['msc'].illustration}
-                actiongauche={proprietesDeVariantsHero['msc'].actiongauche}
-                actiondroite={proprietesDeVariantsHero['msc'].actiondroite}
-                partenaires={proprietesDeVariantsHero['msc'].partenaires}
-    />
-  </Hst.Variant>
+  <OutilSelecteurTheme themeSelectionne="MonServiceSécurisé" />
+  <BriqueHero {titre} {badge} {soustitre} {illustration} {actiongauche} {actiondroite} {partenaires} />
+
+  <svelte:fragment slot="controls">
+    <Hst.Checkbox title="Badge" bind:value={badge} />
+    <Hst.Text title="Titre" bind:value={titre} />
+    <Hst.Text title="Sous titre" bind:value={soustitre} />
+    <Hst.Json title="Illustration" bind:value={illustration} />
+    <Hst.Json title="Action Gauche" bind:value={actiongauche} />
+    <Hst.Json title="Action Droite" bind:value={actiondroite} />
+    <Hst.Json title="Partenaires" bind:value={partenaires} />
+  </svelte:fragment>
 </Hst.Story>
