aae90e1cc5e844b78c8d836fedaee27d8aec0ca9
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2025-05-20 12:10:52 +0200

    Remplaces les "variantes" de la brique Multimédia par des contrôles d'Histoire
    ... afin de nettoyer la story et rendre dynamique le rendu du composant

--- stories/lab/vitrines-produits/briques/BriqueTitreMultimedia.story.svelte
+++ stories/lab/vitrines-produits/briques/BriqueTitreMultimedia.story.svelte
@@ -6,36 +6,22 @@
 
   export let Hst: Hst;
 
+  let titre = 'Titre pour MSS';
   let source = 'https://monservicesecurise-ressources.cellar-c2.services.clever-cloud.com/Video_MonServiceSecurise_Thumbnail.mp4';
   let sousTitres = 'https://monservicesecurise-ressources.cellar-c2.services.clever-cloud.com/Nouvelle_doctrine_homologation.vtt';
-
+  let couverture = genereImageDePlaceholder(792, 446, "Vidéo d'exemple");
 </script>
 
 <Hst.Story title="Composants/Lab/Sites vitrines/Brique Titre Multimédia" icon="material-symbols:brick-outline">
-    <Hst.Variant title="MSS">
-      <OutilSelecteurTheme themeSelectionne="MonServiceSécurisé" />
-      <BriqueTitreMultimedia
-        titre="Titre pour MSS"
-        multimedia={{ source, sourcesoustitres: sousTitres, imagedecouverture: genereImageDePlaceholder(792, 446, "Vidéo d'exemple") }}
-      />
-    </Hst.Variant>
-  <Hst.Variant title="MAC">
-    <OutilSelecteurTheme themeSelectionne="MonAideCyber" />
-    <BriqueTitreMultimedia
-      titre="Titre pour MAC"
-      multimedia={{ source, sourcesoustitres: sousTitres, imagedecouverture: genereImageDePlaceholder(792, 446, "Vidéo d'exemple") }}
-    />
-  </Hst.Variant>
-  <Hst.Variant title="MSC">
-    <OutilSelecteurTheme themeSelectionne="MesServicesCyber" />
-    <BriqueTitreMultimedia
-      titre="Titre pour MSC"
-      multimedia={{ source, sourcesoustitres: sousTitres, imagedecouverture: genereImageDePlaceholder(792, 446, "Vidéo d'exemple") }}
-    />
-  </Hst.Variant>
-
+  <OutilSelecteurTheme themeSelectionne="MonServiceSécurisé" />
+  <BriqueTitreMultimedia
+    {titre}
+    multimedia={{ source, sourcesoustitres: sousTitres, imagedecouverture: couverture }}
+  />
   <svelte:fragment slot="controls">
-    <Hst.Text bind:value={source} title="Lien vers un fichier .mp4" />
-    <Hst.Text bind:value={sousTitres} title="Lien vers les sous titres" />
+    <Hst.Text title="Titre" bind:value={titre} />
+    <Hst.Text title="Lien MP4" bind:value={source}  />
+    <Hst.Text title="Lien sous-titres" bind:value={sousTitres}  />
+    <Hst.Text title="Placeholder" bind:value={couverture}  />
   </svelte:fragment>
 </Hst.Story>
