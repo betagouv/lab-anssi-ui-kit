ac8ba080dc4bdb077028dad8cdb20e3cdab5f908
Marc ETOURNEAU <marc.etourneau@beta.gouv.fr>
2025-05-22 17:47:00 +0200

    Créé la brique réutilisable Contenu à deux colonnes

---
+++ src/lib/lab/vitrines-produits/briques/BriqueContenuADeuxColonnes.svelte
@@ -0,0 +1,134 @@
+
+
+<script lang="ts">
+  import Brique from "$lib/lab/vitrines-produits/briques/Brique.svelte";
+  import type { Action, Image } from "$lib/types";
+
+  export let titre: string;
+  export let paragraphe: string;
+  export let action: Action | undefined;
+
+  export let illustration: Image;
+</script>
+
+<Brique variation="primaire">
+  <div class="grille-contenu">
+    <div class="illustration">
+      <img src={illustration.lien} alt={illustration.alt} />
+    </div>
+    <div class="contenu">
+      <h2>{titre}</h2>
+      <p>{paragraphe}</p>
+      <div class="action">
+        {#if action}
+          <a role="button" href={action.lien} target="_blank">
+            {action.titre}
+          </a>
+        {/if}
+      </div>
+    </div>
+  </div>
+</Brique>
+
+<style lang="scss">
+  .grille-contenu {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-areas: 'illustration'
+                          'contenu';
+    gap: 32px;
+
+    @include a-partir-de(desktop) {
+      grid-template-columns: 1fr 1fr;
+      grid-template-areas: 'contenu illustration';
+      column-gap: 24px;
+    }
+
+    .contenu {
+      grid-area: contenu;
+      display: flex;
+      flex-direction: column;
+      row-gap: 8px;
+
+      @include a-partir-de(desktop) {
+        justify-content: center;
+      }
+
+      h2 {
+        font-size: 1.75rem;
+        line-height: 2.25rem;
+        font-weight: 700;
+        word-break: break-word;
+
+        margin: 0;
+      }
+
+      p {
+        font-size: 1rem;
+        font-weight: 400;
+        line-height: 1.5rem;
+
+        margin: 0;
+      }
+
+      .action {
+        a[role='button'] {
+          display: flex;
+          justify-content: center;
+          text-decoration: none;
+          font-family: Marianne, sans-serif;
+          padding: 8px 16px;
+          max-width: 100%;
+
+          text-align: center;
+          font-weight: 500;
+          font-size: 1rem;
+          line-height: 24px;
+
+          border-radius: 4px;
+
+          background-color: $brique-contenu-deux-colonnes-action-bouton-background;
+          color: $brique-contenu-deux-colonnes-action-bouton-texte;
+          border: none;
+          cursor: pointer;
+
+          &:active {
+            background-color: $brique-contenu-deux-colonnes-action-bouton-background-active;
+            box-shadow: none;
+            border: none;
+          }
+
+          &:hover {
+            background-color: $brique-contenu-deux-colonnes-action-bouton-background-hover;
+            box-shadow: none;
+            border: none;
+          }
+
+          @include a-partir-de(tablette) {
+            max-width: unset;
+            width: fit-content;
+          }
+        }
+
+        margin-top: 32px;
+      }
+    }
+
+    .illustration {
+      grid-area: illustration;
+      img {
+        width: 100%;
+        max-height: 250px;
+
+        @include a-partir-de(tablette) {
+          max-height: none;
+        }
+
+        @include a-partir-de(desktop) {
+          max-height: 330px;
+        }
+      }
+    }
+
+  }
+</style>
---
+++ stories/lab/vitrines-produits/briques/BriqueContenuADeuxColonnes.story.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import type { Hst } from "@histoire/plugin-svelte";
+  import OutilSelecteurTheme from "../../../OutilSelecteurTheme.svelte";
+  import { genereImageDePlaceholder } from "$lib/generateurImagesPlaceholders";
+  import BriqueContenuADeuxColonnes from "$lib/lab/vitrines-produits/briques/BriqueContenuADeuxColonnes.svelte";
+
+  export let Hst: Hst;
+
+  let titre = "Mon​Service​Sécurisé";
+  let paragraphe = "L'outil pour piloter en équipe la sécurité de tous vos services numériques et les homologuer rapidement";
+  let illustration = { lien: genereImageDePlaceholder(600, 400, 'Placeholder'), alt: "Logo placeholder" };
+  let action = { titre: "Commencer à sécuriser", lien: "#" };
+</script>
+
+<Hst.Story title="Composants/Lab/Sites vitrines/Brique Contenu à deux colonnes" icon="material-symbols:brick-outline">
+  <OutilSelecteurTheme themeSelectionne="MonServiceSécurisé" />
+  <BriqueContenuADeuxColonnes {titre} {paragraphe} {illustration} {action} />
+
+  <svelte:fragment slot="controls">
+    <Hst.Text title="Titre" bind:value={titre} />
+    <Hst.Text title="Texte" bind:value={paragraphe} />
+    <Hst.Json title="Illustration" bind:value={illustration} />
+    <Hst.Json title="Action" bind:value={action} />
+  </svelte:fragment>
+</Hst.Story>
