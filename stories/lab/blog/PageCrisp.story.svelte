42fd620886818d0cfea1b2151bbceda8667ee956
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-06-03 10:36:22 +0200

    Ajoute un composant `PageCrisp`

---
+++ stories/lab/blog/PageCrisp.story.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import type {Hst} from "@histoire/plugin-svelte";
+    import PageCrisp from "$lib/lab/blog/PageCrisp.svelte";
+    import OutilSelecteurTheme from "../../OutilSelecteurTheme.svelte";
+
+    export let Hst: Hst;
+
+</script>
+
+<Hst.Story title="Composants/Lab/Blog/Page Crisp" icon="uis:web-grid-alt">
+  <OutilSelecteurTheme />
+  <PageCrisp/>
+</Hst.Story>
