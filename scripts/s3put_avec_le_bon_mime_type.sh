c4048dd2b2735211f1677fab92d66b6261ada117
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2025-06-13 15:33:29 +0200

    Ajoute une Github Actions permettant de déployer un environnement de "DEV" du storybook

---
+++ scripts/s3put_avec_le_bon_mime_type.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+extension() {
+  local file_name="$1"
+  local extension
+  extension=$(echo "$file_name" | sed -e 's:.*\.\([^.]*\)$:\1:')
+  echo "$extension"
+}
+
+mime_type() {
+  local file="$1"
+  local ext
+  ext="$(extension "$file")"
+
+  case $ext in
+    js)    echo text/javascript
+           ;;
+    css)   echo text/css
+           ;;
+    html)  echo text/html
+           ;;
+    woff)  echo font/woff
+           ;;
+    woff2) echo font/woff
+           ;;
+    scg)   echo image/svg+xml
+           ;;
+    *)     file --mime-type $file | cut -d: -f2 | tr -d ' '
+  esac
+}
+
+for file in $(find dist-histoire -type f)
+do
+  mime="$(mime_type "$file")"
+  target="$(echo "$file" | sed -e s:^dist-histoire/::)"
+  s3cmd put "$file" "s3://lab-anssi-ui-kit-dev/$target" --mime-type="$mime"
+done
