820fa49819ce7d1829b092f64f0b2347db983b16
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-07-01 17:11:02 +0200

    Permets de choisir la taille des icônes

--- src/lib/lab/Icone.svelte
+++ src/lib/lab/Icone.svelte
@@ -1,8 +1,19 @@
+<svelte:options
+  customElement={{
+    tag: "lab-anssi-icone",
+    props: {
+      nom: { reflect: false, type: "String", attribute: "nom" },
+      taille: { reflect: false, type: "String", attribute: "taille" },
+    },
+  }}
+/>
+
 <script lang="ts">
-    export let nom: string;
+  export let nom: string;
+  export let taille: "sm" | "md" | "lg" = "md";
 </script>
 
-<span class="icone fr-icon-{nom}"></span>
+<span class="icone fr-icon-{nom} {taille}"></span>
 
 <style lang="scss">
   .icone {
@@ -10,15 +21,32 @@
 
     &:before {
       content: "";
-      --icon-size: 1.5rem;
       background-color: currentColor;
       display: inline-block;
       flex: 0 0 auto;
       -webkit-mask-size: 100% 100%;
       mask-size: 100% 100%;
-      vertical-align: calc((.75em - var(--icon-size)) * .5);
+      vertical-align: calc((0.75em - var(--icon-size)) * 0.5);
       width: var(--icon-size);
       height: var(--icon-size);
+    }
+
+    &.sm {
+      &:before {
+        --icon-size: 1rem;
+      }
+    }
+
+    &.md {
+      &:before {
+        --icon-size: 1.5rem;
+      }
+    }
+
+    &.lg {
+      &:before {
+        --icon-size: 2rem;
+      }
     }
   }
 
--- stories/lab/Icone.story.svelte
+++ stories/lab/Icone.story.svelte
@@ -1,22 +1,42 @@
 <script lang="ts">
-    import type {Hst} from "@histoire/plugin-svelte";
-    import Icone from "$lib/lab/Icone.svelte";
+  import type { Hst } from "@histoire/plugin-svelte";
+  import Icone from "$lib/lab/Icone.svelte";
 
-    export let Hst: Hst;
+  export let Hst: Hst;
 
-    let nom: string = "leaf-line";
+  let nom: string = "leaf-line";
+  let taille: string = "md";
+  let couleur: string = "red";
+  const taillesDisponibles = [
+    { value: "sm", label: "Petite (1 rem)" },
+    { value: "md", label: "Moyenne (1.5 rem)" },
+    { value: "lg", label: "Grande  (2 rem)" },
+  ];
 </script>
 
 <Hst.Story title="Composants/Lab/Icone">
   <div style="background-color:white; padding:48px;">
     <h3>Icône</h3>
-    <Icone {nom}/>
+    <p>
+      Les icônes disponibles et leur code sont sur la page du <a
+        href="https://www.systeme-de-design.gouv.fr/version-courante/fr/fondamentaux/icone"
+        target="_blank">DSFR</a
+      >. Les icônes du DSFR sont elles-mêmes issues de
+      <a href="https://remixicon.com/" target="_blank">Remix Icons</a>.
+    </p>
+    <Icone {nom} {taille} />
     <h4>Avec une couleur</h4>
-    <span style="color:red">
-      <Icone {nom}/>
+    <p>
+      Les icônes sont au format SVG avec la couleur « currentColor ». Ces fichiers SVG sont utilisés
+      comme image de masque et peuvent donc hériter de la couleur de leur élément parent :
+    </p>
+    <span style="color:{couleur}">
+      <Icone {nom} {taille} />
     </span>
   </div>
   <svelte:fragment slot="controls">
-    <Hst.Text title="Nom" bind:value={nom}/>
+    <Hst.Text title="Nom" bind:value={nom} />
+    <Hst.Select title="Taille" options={taillesDisponibles} bind:value={taille} />
+    <Hst.Text title="Couleur" bind:value={couleur} />
   </svelte:fragment>
 </Hst.Story>
