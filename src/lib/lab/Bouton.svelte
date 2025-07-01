6f4f94c12fe949b9dbeec62e019fb40e8a5c1ca7
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-07-01 17:57:10 +0200

    Ajoute le petit bouton tertiaire

---
+++ src/lib/lab/Bouton.svelte
@@ -0,0 +1,73 @@
+<svelte:options
+  customElement={{
+    tag: "lab-anssi-bouton",
+    props: {
+      titre: { reflect: false, type: "String", attribute: "titre" },
+      variante: { reflect: false, type: "String", attribute: "variante" },
+      taille: { reflect: false, type: "String", attribute: "taille" },
+      icone: { reflect: false, type: "String", attribute: "icone" },
+      sansBordure: { reflect: false, type: "Boolean", attribute: "sans-bordure" },
+    },
+  }}
+/>
+
+<script lang="ts">
+  import Icone from "$lib/lab/Icone.svelte";
+
+  export let titre: string;
+  export let variante = "tertiaire";
+  export let taille = "sm";
+  export let icone: string | undefined = undefined;
+  export let sansBordure = false;
+</script>
+
+<button class={`bouton ${variante} ${taille}`} class:sansBordure on:click>
+  {titre}
+  {#if icone}
+    <Icone nom={icone} taille="sm" />
+  {/if}
+</button>
+
+<style>
+  .bouton {
+    display: flex;
+    align-items: center;
+    gap: 8px;
+    font-family: Marianne, arial, sans-serif;
+    cursor: pointer;
+
+    &.tertiaire {
+      align-items: center;
+      color: #0d0c21;
+      text-align: center;
+      background: white;
+      border: 1px solid #ddd;
+      font-size: 0.875rem;
+      font-style: normal;
+      font-weight: 500;
+      line-height: 1.5rem;
+      border-radius: 4px;
+
+      &.sm {
+        padding: 4px 12px;
+      }
+
+      &.sansBordure {
+        border: none;
+      }
+
+      &:hover {
+        background: rgb(0, 0, 0, 4%);
+      }
+
+      &:active {
+        background: rgb(0, 0, 0, 8%);
+      }
+
+      &:focus {
+        outline: 2px solid #0a76f6;
+        outline-offset: 2px;
+      }
+    }
+  }
+</style>
---
+++ stories/lab/Bouton.story.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import type { Hst } from "@histoire/plugin-svelte";
+  import Bouton from "$lib/lab/Bouton.svelte";
+
+  export let Hst: Hst;
+
+  let variante: string = "tertiaire";
+  let taille: string = "sm";
+  let icone: string = "leaf-line";
+
+  const variantesDisponibles = [{ value: "tertiaire", label: "Tertiaire" }];
+  const taillesDisponibles = [{ value: "sm", label: "Petit" }];
+</script>
+
+<Hst.Story title="Composants/Lab/Bouton">
+  <div style="background-color:white; padding:48px;">
+    <h3>Bouton</h3>
+    <Bouton titre="Libellé" {variante} {taille} {icone} />
+    <h4>Tertiaire sans bordure</h4>
+    <Bouton titre="Libellé" variante="tertiaire" {taille} {icone} sansBordure />
+  </div>
+  <svelte:fragment slot="controls">
+    <Hst.Text title="Icône" bind:value={icone} />
+    <Hst.Select title="Variante" options={variantesDisponibles} bind:value={variante} />
+    <Hst.Select title="Taille" options={taillesDisponibles} bind:value={taille} />
+  </svelte:fragment>
+</Hst.Story>
