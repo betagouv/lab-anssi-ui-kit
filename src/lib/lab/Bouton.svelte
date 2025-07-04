30981aa871eadc4502b207ca068395cbb48ff246
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-07-04 14:43:57 +0200

    [SOIN] Ajoute une variante pour le tertiaire sans bordure
    ...qui s’appelle 'tertiaire-sans-bordure' à la place d’une propriété supplémentaire

--- src/lib/lab/Bouton.svelte
+++ src/lib/lab/Bouton.svelte
@@ -6,7 +6,6 @@
       variante: { reflect: false, type: "String", attribute: "variante" },
       taille: { reflect: false, type: "String", attribute: "taille" },
       icone: { reflect: false, type: "String", attribute: "icone" },
-      sansBordure: { reflect: false, type: "Boolean", attribute: "sans-bordure" },
       positionIcone: { reflect: false, type: "String", attribute: "position-icone" },
     },
   }}
@@ -16,14 +15,13 @@
   import Icone from "$lib/lab/Icone.svelte";
 
   export let titre: string;
-  export let variante: "primaire" | "tertiaire";
+  export let variante: "primaire" | "tertiaire" | "tertiaire-sans-bordure";
   export let taille: "sm" | "md" | "lg";
   export let icone: string | undefined = undefined;
-  export let sansBordure = false;
   export let positionIcone: "sans" | "seule" | "droite" | "gauche" = "sans";
 </script>
 
-<button class={`bouton ${variante} ${taille} icone-${positionIcone}`} class:sansBordure on:click>
+<button class={`bouton ${variante} ${taille} icone-${positionIcone}`} on:click>
   {#if (positionIcone === "gauche" || positionIcone === "seule") && icone}
     <Icone nom={icone} taille={taille === "lg" ? "md" : "sm"} />
   {/if}
