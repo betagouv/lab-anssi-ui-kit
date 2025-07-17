9e18e346c33781ace9a020d7da0c8a6c588c1f8c
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2025-07-17 12:08:22 +0200

    Crée la story du composant Tag

---
+++ stories/lab/Tag.story.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import Tag from "$lib/lab/Tag.svelte";
+  import type { Hst } from "@histoire/plugin-svelte";
+
+  export let Hst: Hst;
+
+  let label: string = "libellé";
+  let couleurTexte: string = "#FFF";
+  let couleurFond: string = "#000";
+</script>
+
+<Hst.Story title="Composants/Lab/Tag">
+  <div style="background-color:white; padding:48px;">
+    <h3>Tag</h3>
+    <Tag {label} {couleurTexte} {couleurFond} />
+  </div>
+
+  <svelte:fragment slot="controls">
+    <Hst.ColorSelect title="Couleur Texte" bind:value={couleurTexte} />
+    <Hst.ColorSelect title="Couleur Fond" bind:value={couleurFond} />
+    <Hst.Text title="Libellé" bind:value={label} />
+  </svelte:fragment>
+</Hst.Story>
