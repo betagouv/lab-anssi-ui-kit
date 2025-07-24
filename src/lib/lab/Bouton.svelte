bcc130043d4cf4292834f439af378a2639efcc87
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-07-24 16:42:14 +0200

    Ajoute l’état désactivé du bouton

--- src/lib/lab/Bouton.svelte
+++ src/lib/lab/Bouton.svelte
@@ -7,6 +7,7 @@
       taille: { reflect: false, type: "String", attribute: "taille" },
       icone: { reflect: false, type: "String", attribute: "icone" },
       positionIcone: { reflect: false, type: "String", attribute: "position-icone" },
+      actif: { reflect: false, type: "Boolean", attribute: "actif" },
     },
   }}
 />
@@ -19,12 +20,14 @@
   export let taille: "sm" | "md" | "lg";
   export let icone: string | undefined = undefined;
   export let positionIcone: "sans" | "seule" | "droite" | "gauche" = "sans";
+  export let actif: boolean = true;
 </script>
 
 <button
   class={`bouton ${variante} ${taille} icone-${positionIcone}`}
   on:click
   title={positionIcone === "seule" ? titre : ""}
+  disabled={!actif}
 >
   {#if (positionIcone === "gauche" || positionIcone === "seule") && icone}
     <Icone nom={icone} taille={taille === "lg" ? "md" : "sm"} />
