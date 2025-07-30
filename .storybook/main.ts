dc584be989546e3c2c85d56ecf6ba60fad6e4b56
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2025-07-30 11:08:33 +0200

    Ajoute une première story

---
+++ .storybook/main.ts
@@ -0,0 +1,46 @@
+import path from "path";
+import type { StorybookConfig } from "@storybook/sveltekit";
+import { loadEnv } from "vite";
+
+const varEnv = loadEnv(process.env.HISTOIRE_ENV ?? "production", process.cwd(), "VITE_");
+
+const config: StorybookConfig = {
+  stories: ["../stories_storybook/**/*.mdx", "../stories_storybook/**/*.stories.@(js|ts|svelte)"],
+  addons: [
+    "@storybook/addon-svelte-csf",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/sveltekit",
+    options: {},
+  },
+  viteFinal: async (config) => {
+    config.server = {
+      ...config.server,
+      fs: {
+        ...config.server?.fs,
+        allow: [...(config.server?.fs?.allow || []), "./stories_storybook", "./src"],
+      },
+    };
+    config.css = {
+      ...config.css,
+      preprocessorOptions: {
+        ...config.preprocessorOptions,
+        scss: {
+          ...config.preprocessorOptions?.scss,
+          additionalData: `
+            @use '${path.resolve(__dirname, '../src/variables.scss')}' as *;
+            @use '${path.resolve(__dirname, '../src/responsive.scss')}' as *;
+            @use '${path.resolve(__dirname, '../src/assets.scss')}' as *;
+            $assets-url-base: '${varEnv.VITE_LAB_ANSSI_UI_KIT_ASSET_BASE}';
+          `,
+        },
+      },
+    };
+    return config;
+  },
+};
+export default config;
