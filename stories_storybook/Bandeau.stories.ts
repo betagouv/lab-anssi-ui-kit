79e2cf62514c2a3a09d82eebb5df8efcd5d2e81e
Trésor ILOYI <tresor.iloyi@gmail.com>
2025-07-30 15:38:42 +0200

    Ajoute une story pour le composant Bandeau

---
+++ stories_storybook/Bandeau.stories.ts
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from "@storybook/svelte";
+import { expect, userEvent } from "@storybook/test";
+
+import Bandeau from "$lib/mes-services-cyber/bandeau/Bandeau.svelte";
+
+const meta = {
+  title: "Composants/Bandeau",
+  component: Bandeau,
+  tags: ["autodocs"],
+  play: async ({ canvas, canvasElement }) => {
+    const button = canvas.getByRole("button");
+    await userEvent.click(button);
+    await expect(canvasElement.querySelector("div.largeur-totale")).not.toBeInTheDocument();
+  },
+} satisfies Meta<Bandeau>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Defaut: Story = {}
