1f9781f33540f0b1d9e05de20e55f15676035244
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2025-07-30 11:08:33 +0200

    Ajoute une première story

---
+++ stories_storybook/Alerte.stories.ts
@@ -0,0 +1,42 @@
+import type { Meta, StoryObj } from "@storybook/svelte";
+import { expect, userEvent } from "@storybook/test";
+
+import Alerte from "$lib/Alerte.svelte";
+
+const meta = {
+  title: "Composants/Alerte",
+  component: Alerte,
+  tags: ["autodocs"],
+  argTypes: {
+    description: { control: "text", type: "string" },
+    type: {
+      control: { type: "select" },
+      options: ["information", "erreur"],
+    },
+    fermable: { control: "boolean", type: "boolean" },
+  },
+  play: async ({ canvas, canvasElement }) => {
+    const button = canvas.getByRole("button");
+    await userEvent.click(button);
+    await expect(canvasElement.querySelector("div")).not.toBeInTheDocument();
+  },
+} satisfies Meta<Alerte>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Information: Story = {
+  args: {
+    description: "Une alerte d'information",
+    type: "information",
+    fermable: true,
+  },
+};
+
+export const Erreur: Story = {
+  args: {
+    description: "Une alerte d'erreur",
+    type: "erreur",
+    fermable: true,
+  },
+};
