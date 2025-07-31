b6f10b05d69d87c2a0e445ef7afb584f608ab66a
ThibaudMZN <thibaud.marazano@beta.gouv.fr>
2025-07-31 16:01:05 +0200

    Ajoute une story pour la Navigation de la suite cyber

---
+++ stories_storybook/NavigationSuiteCyber.stories.svelte
@@ -0,0 +1,24 @@
+<script context="module">
+  import ConteneurStory from "./ConteneurStory.svelte";
+  import Navigation from "$lib/suite-cyber/navigation/Navigation.svelte";
+
+  export const meta = {
+    title: "Composants/Navigation Suite Cyber",
+    component: Navigation,
+    args: {
+      sourceUtm: "mon-service-lab-anssi",
+    },
+  };
+</script>
+
+<script>
+  import { Story, Template } from "@storybook/addon-svelte-csf";
+</script>
+
+<Template let:args>
+  <ConteneurStory alignement="droite">
+    <Navigation {...args} />
+  </ConteneurStory>
+</Template>
+
+<Story name="Defaut" />
