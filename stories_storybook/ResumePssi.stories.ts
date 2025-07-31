0b4abd0fdbb0db5497bd09162d7075b8c3249e9b
Trésor ILOYI <tresor.iloyi@gmail.com>
2025-07-31 18:05:57 +0200

    Ajoute la story du composant ResumePssi

---
+++ stories_storybook/ResumePssi.stories.ts
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from "@storybook/svelte";
+
+import ResumePssi from "$lib/lab/ResumePssi.svelte";
+
+const meta = {
+  title: "Composants/ResumePssi",
+  component: ResumePssi,
+  args: {
+    nomService: "MonService",
+  },
+  argTypes: {
+    nomService: { control: "text", description: "Le nom du service" },
+  },
+} satisfies Meta<ResumePssi>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Defaut: Story = {};
