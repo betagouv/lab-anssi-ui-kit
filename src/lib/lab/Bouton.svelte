de646e2ec2c1ad377afb437f5acecba5b9442081
Trésor ILOYI <tresor.iloyi@gmail.com>
2025-08-05 16:54:14 +0200

    Migre le composant Bouton vers Svelte 5

--- src/lib/lab/Bouton.svelte
+++ src/lib/lab/Bouton.svelte
@@ -16,13 +16,25 @@
 <script lang="ts">
   import Icone from "$lib/lab/Icone.svelte";
 
-  export let titre: string;
-  export let variante: "primaire" | "secondaire" | "tertiaire" | "tertiaire-sans-bordure";
-  export let taille: "sm" | "md" | "lg";
-  export let icone: string | undefined = undefined;
-  export let positionIcone: "sans" | "seule" | "droite" | "gauche" = "sans";
-  export let actif: boolean = true;
-  export let largeurMaximale: boolean = false;
+  interface Props {
+    titre: string;
+    variante: "primaire" | "tertiaire" | "tertiaire-sans-bordure";
+    taille: "sm" | "md" | "lg";
+    icone?: string | undefined;
+    positionIcone?: "sans" | "seule" | "droite" | "gauche";
+    actif?: boolean;
+    largeurMaximale?: boolean;
+  }
+
+  let {
+    titre,
+    variante,
+    taille,
+    icone = undefined,
+    positionIcone = "sans",
+    actif = true,
+    largeurMaximale = false,
+  }: Props = $props();
 </script>
 
 <button
