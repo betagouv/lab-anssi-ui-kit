3ad97743608bc8f511e417119f1b036af7306502
Thomas Lechat <thomas.lechat@beta.gouv.fr>
2025-08-05 11:34:52 +0200

    Rends l'injection de nonce plus robuste
    ...suite au passage à svelte 5

--- outils/injection-nonce.ts
+++ outils/injection-nonce.ts
@@ -1,9 +1,14 @@
 export const injecteNonceWebcomponents = (code: string) => {
   let codeAvecNonce = `const nonce = document.currentScript?.nonce;\n${code}`;
-  codeAvecNonce = codeAvecNonce.replace(
-    /const (.)\s*=\s*.\("style"\);/gm,
-    (match, nomVariable) => `${match}${nomVariable}.nonce=nonce;`,
-  );
+  codeAvecNonce = codeAvecNonce
+    .replace(
+      /const (.)\s*=\s*.\(["']style["']\);/gm,
+      (match, nomVariable) => `${match}${nomVariable}.nonce=nonce;`,
+    )
+    .replace(
+      /const (.)\s*=\s*document\.createElement\(["']style["']\);/gm,
+      (match, nomVariable) => `${match}${nomVariable}.nonce=nonce;`,
+    );
 
   return codeAvecNonce;
 };
