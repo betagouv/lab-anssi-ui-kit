dd23cb897173dca2c96261ce6c8d66dd78da466e
Trésor ILOYI <tresor.iloyi@gmail.com>
2025-08-20 09:32:28 +0200

    [COMPOSANT] Ajoute le composant Filtres Catalogue du Lab

---
+++ stories/composants/anssi/Filtres.stories.svelte
@@ -0,0 +1,13 @@
+<script module>
+  import { defineMeta } from "@storybook/addon-svelte-csf";
+  import FiltresCatalogue from "$lib/composants/FiltresCatalogue.svelte";
+
+  const { Story } = defineMeta({
+    component: FiltresCatalogue,
+    title: "Composants/ANSSI/Filtres Catalogue",
+  });
+</script>
+
+<Story name="Défaut" />
+
+<Story name="Horizontal" args={{ horizontal: true }} />
