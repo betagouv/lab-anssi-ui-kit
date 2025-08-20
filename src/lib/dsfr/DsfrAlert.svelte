2e6e9eedd3e6410677b36e9b287d62f97d0413a1
Trésor ILOYI <tresor.iloyi@gmail.com>
2025-08-20 07:03:01 +0200

    [COMPOSANT] Ajoute le composant Alert du DSFR

---
+++ src/lib/dsfr/DsfrAlert.svelte
@@ -0,0 +1,73 @@
+<svelte:options
+  customElement={{
+    tag: "dsfr-alert",
+    props: {
+      buttonCloseLabel: { attribute: "button-close-label", type: "String" },
+      hasTitle: { attribute: "has-title", type: "Boolean" },
+      title: { attribute: "title", type: "String" },
+      hasDescription: { attribute: "has-description", type: "Boolean" },
+      text: { attribute: "text", type: "String" },
+      type: { attribute: "type", type: "String" },
+      size: { attribute: "size", type: "String" },
+      id: { attribute: "id", type: "String" },
+      dismissible: { attribute: "dismissible", type: "Boolean" },
+      icon: { attribute: "icon", type: "String" },
+    },
+  }}
+/>
+
+<script lang="ts">
+  import { setIconClass } from "$lib/utilitaires";
+  import type { Size } from "$lib/types";
+
+  type AlertSize = Extract<Size, "sm" | "md">;
+
+  interface Props {
+    buttonCloseLabel?: string;
+    hasTitle?: boolean;
+    title?: string;
+    hasDescription?: boolean;
+    text?: string;
+    type?: "default" | "success" | "error" | "info" | "warning";
+    size?: AlertSize;
+    id?: string;
+    dismissible?: boolean;
+    icon?: string;
+  }
+
+  let {
+    hasTitle = true,
+    hasDescription = true,
+    title,
+    text,
+    type = "default",
+    size = "md",
+    id,
+    icon,
+    dismissible = false,
+    buttonCloseLabel,
+  }: Props = $props();
+
+  let iconClass = $derived(setIconClass(icon));
+  let typeClass = $derived(`fr-alert--${type}`);
+  let sizeClass = $derived(`fr-alert--${size}`);
+</script>
+
+<div {id} class={["fr-alert", iconClass, typeClass, sizeClass]}>
+  {#if hasTitle || size === "md"}
+    <h3 class="fr-alert__title">{title}</h3>
+  {/if}
+  {#if hasDescription || size === "sm"}
+    <p>{text}</p>
+  {/if}
+  {#if dismissible}
+    <button title={buttonCloseLabel} type="button" class="fr-btn--close fr-btn">
+      {buttonCloseLabel}
+    </button>
+  {/if}
+</div>
+
+<style lang="scss">
+  @import "@gouvfr/dsfr/dist/core/core.min.css";
+  @import "@gouvfr/dsfr/dist/component/alert/alert.min.css";
+</style>
