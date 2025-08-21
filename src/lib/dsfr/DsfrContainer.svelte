eb668813f55b6531c858a6cefb3f3526fb1931f8
Trésor ILOYI <tresor.iloyi@gmail.com>
2025-08-21 10:59:43 +0200

    [COMPOSANT] Ajoute le composant Container du DSFR

---
+++ src/lib/dsfr/DsfrContainer.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  interface Props {
+    /**
+     * Permet de définir le conteneur comme "fluide" ou non
+     */
+    fluid?: boolean;
+
+    /**
+     * Permet de définir le contenu du conteneur
+     */
+    children?: import("svelte").Snippet;
+  }
+
+  let { fluid = false, children }: Props = $props();
+</script>
+
+<div class={[!fluid && "fr-container", fluid && "fr-container--fluid"]}>
+  {@render children?.()}
+</div>
+
+<style lang="scss">
+  @import "@gouvfr/dsfr/dist/core/core.min.css";
+
+  .fr-container {
+    &,
+    &--fluid {
+      box-sizing: border-box;
+    }
+  }
+</style>
---
+++ stories/dsfr/Container.stories.svelte
@@ -0,0 +1,54 @@
+<script module>
+  import { defineMeta } from "@storybook/addon-svelte-csf";
+  import DsfrContainer from "$lib/dsfr/DsfrContainer.svelte";
+
+  const { Story } = defineMeta({
+    title: "Composants/dsfr/Container",
+    component: DsfrContainer,
+  });
+</script>
+
+<Story name="Défaut">
+  {#snippet template(args)}
+    <DsfrContainer {...args}>
+      <p>Contenu du conteneur</p>
+      <small>
+        <i>
+          La bordure présente dans cette story sert uniquement à titre d'illustration et n'est pas
+          présente lors de l'usage classique du composant
+        </i>
+      </small>
+    </DsfrContainer>
+    <style>
+      .fr-container {
+        border: 1px dashed black;
+        min-height: 24px;
+      }
+    </style>
+  {/snippet}
+</Story>
+
+<Story
+  name="Fluid"
+  args={{
+    fluid: true,
+  }}
+>
+  {#snippet template(args)}
+    <DsfrContainer {...args}>
+      <p>Contenu du conteneur fluide</p>
+      <small>
+        <i>
+          La bordure présente dans cette story sert uniquement à titre d'illustration et n'est pas
+          présente lors de l'usage classique du composant
+        </i>
+      </small>
+    </DsfrContainer>
+    <style>
+      .fr-container--fluid {
+        border: 1px dashed black;
+        min-height: 24px;
+      }
+    </style>
+  {/snippet}
+</Story>
