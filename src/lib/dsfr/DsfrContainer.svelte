04e3de822796155704c1075c66a02dc91ced19e6
Trésor ILOYI <tresor.iloyi@gmail.com>
2025-08-21 10:59:43 +0200

    [COMPOSANT] Ajoute le composant Container du DSFR

---
+++ src/lib/dsfr/DsfrContainer.svelte
@@ -0,0 +1,39 @@
+<svelte:options
+  customElement={{
+    tag: "dsfr-container",
+    props: {
+      fluid: { attribute: "fluid", type: "Boolean" },
+    },
+  }}
+/>
+
+<script lang="ts">
+  interface Props {
+    /**
+     * Permet de définir le conteneur comme "fluide" ou non
+     */
+    fluid?: boolean;
+
+    /**
+     * Permet de définir le contenu du conteneur
+     */
+    children?: import("svelte").Snippet;
+  }
+
+  let { fluid = false, children }: Props = $props();
+</script>
+
+<div class={[!fluid && "fr-container", fluid && "fr-container--fluid"]}>
+  {@render children?.()}
+</div>
+
+<style lang="scss">
+  @import "@gouvfr/dsfr/dist/core/core.min.css";
+
+  .fr-container {
+    &,
+    &--fluid {
+      box-sizing: border-box;
+    }
+  }
+</style>
