f6b014d426149452a167a7a93611a9e4513c3bae
Trésor ILOYI <tresor.iloyi@gmail.com>
2025-09-09 22:23:33 +0200

    [COMPOSANT] Ajoute le composant Tag du DSFR

---
+++ stories/dsfr/Tag.stories.svelte
@@ -0,0 +1,17 @@
+<script module>
+  import { defineMeta } from "@storybook/addon-svelte-csf";
+  import {
+    tagArgs,
+    tagArgTypes,
+  } from "@gouvfr/dsfr/src/dsfr/component/tag/template/stories/tag-arg-types.js";
+  import DsfrTag from "$lib/dsfr/DsfrTag.svelte";
+
+  const { Story } = defineMeta({
+    title: "Composants/dsfr/Tag",
+    component: DsfrTag,
+    argTypes: tagArgTypes,
+    args: tagArgs,
+  });
+</script>
+
+<Story name="Défaut" />
