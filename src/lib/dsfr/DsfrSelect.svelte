6f1caca04b74b12142d1790f43928b18eec342e8
Trésor ILOYI <tresor.iloyi@gmail.com>
2025-09-10 07:04:26 +0200

    [COMPOSANT] Ajoute le composant Select du DSFR

---
+++ src/lib/dsfr/DsfrSelect.svelte
@@ -0,0 +1,132 @@
+<svelte:options
+  customElement={{
+    tag: "dsfr-select",
+    props: {
+      id: { attribute: "id", type: "String" },
+      label: { attribute: "label", type: "String" },
+      value: { attribute: "value", type: "String", reflect: true },
+      groupedOptions: { attribute: "grouped-options", type: "Boolean" },
+      options: { attribute: "options", type: "Array" },
+      optionGroups: { attribute: "option-groups", type: "Object" },
+      hint: { attribute: "hint", type: "String" },
+      placeholder: { attribute: "placeholder", type: "String" },
+      disabled: { attribute: "disabled", type: "Boolean" },
+      status: { attribute: "status", type: "String" },
+      errorMessage: { attribute: "error-message", type: "String" },
+      validMessage: { attribute: "valid-message", type: "String" },
+    },
+  }}
+/>
+
+<script lang="ts">
+  import { createEventDispatcher } from "svelte";
+
+  type Option = {
+    value: string;
+    label: string;
+  };
+  type OptionGroup = {
+    label: string;
+    options: Option[];
+  };
+  interface Props {
+    /** Attribut id de la liste déroulante */
+    id: string;
+    /** Libellé de la liste déroulante */
+    label: string;
+    /** Valeur initiale du champ */
+    value?: string;
+    /** Groupe d\'options */
+    groupedOptions?: boolean;
+    /** Options de la liste déroulante */
+    options?: Option[] | undefined;
+    /** Options de la liste déroulante avec groupes */
+    optionGroups: OptionGroup | undefined;
+    /** Texte additionnel sous le label */
+    hint?: string;
+    /** Texte avant sélection d'une option */
+    placeholder?: string;
+    /** Désactive la liste déroulante */
+    disabled?: boolean;
+    /** Statut du message */
+    status?: "default" | "valid" | "error";
+    /** Texte du message d'erreur */
+    errorMessage?: string;
+    /** Texte du message de succès */
+    validMessage?: string;
+  }
+
+  const dispatch = createEventDispatcher();
+
+  let {
+    id,
+    label,
+    value = $bindable(),
+    groupedOptions,
+    options,
+    hint,
+    placeholder,
+    disabled,
+    status = "default",
+    errorMessage,
+    validMessage,
+  }: Props = $props();
+
+  const disabledClass = $derived.by(() => {
+    return disabled && "fr-select-group--disabled";
+  });
+
+  function handleChange(event: Event) {
+    const target = event.target as HTMLInputElement;
+    dispatch("valuechanged", target.value);
+  }
+</script>
+
+<div class={["fr-select-group", disabledClass]}>
+  <label class="fr-label" for={id}>
+    {label}
+
+    {#if hint}
+      <span class="fr-hint-text">{hint}</span>
+    {/if}
+  </label>
+  <select
+    class="fr-select"
+    aria-describedby={status ? `${id}-messages` : undefined}
+    {id}
+    name={id}
+    bind:value
+    {disabled}
+    onchange={handleChange}
+  >
+    {#if placeholder}
+      <option value="" selected disabled>{placeholder}</option>
+    {/if}
+    {#if $$slots.default}
+      <slot></slot>
+    {:else}
+      {#each options as option}
+        <option value={option.value}>
+          {option.label}
+        </option>
+      {/each}
+    {/if}
+  </select>
+
+  {#if status !== "default"}
+    <div class="fr-messages-group" id={status ? `${id}-messages` : undefined} aria-live="polite">
+      <p
+        class={["fr-message", `fr-message--${status}`]}
+        id={status ? `${id}-message-${status}` : undefined}
+      >
+        {validMessage || errorMessage}
+      </p>
+    </div>
+  {/if}
+</div>
+
+<style lang="scss">
+  @use "@gouvfr/dsfr/src/dsfr/core/main" as *;
+  @use "@gouvfr/dsfr/src/dsfr/component/form/main" as *;
+  @use "@gouvfr/dsfr/src/dsfr/component/select/main" as *;
+</style>
