2a620adc660d7f83c57febd8f5d7f4d89607229f
Trésor ILOYI <tresor.iloyi@gmail.com>
2025-09-10 06:12:43 +0200

    [COMPOSANT] Ajoute le composant Checkbox du DSFR

---
+++ src/lib/dsfr/DsfrCheckbox.svelte
@@ -0,0 +1,109 @@
+<svelte:options
+  customElement={{
+    tag: "dsfr-checkbox",
+    props: {
+      id: { attribute: "id", type: "String" },
+      label: { attribute: "label", type: "String" },
+      name: { attribute: "name", type: "String" },
+      size: { attribute: "size", type: "String" },
+      hint: { attribute: "hint", type: "String" },
+      disabled: { attribute: "disabled", type: "Boolean" },
+      checked: { attribute: "checked", type: "Boolean", reflect: true },
+      value: { attribute: "value", type: "String", reflect: true },
+      status: { attribute: "status", type: "String" },
+      errorMessage: { attribute: "error-message", type: "String" },
+      validMessage: { attribute: "valid-message", type: "String" },
+    },
+  }}
+/>
+
+<script lang="ts">
+  import type { Size } from "$lib/types";
+  import { createEventDispatcher } from "svelte";
+
+  type ChecboxSize = Extract<Size, "sm" | "md">;
+  interface Props {
+    /** Attribut id de la checkbox */
+    id: string;
+    /** Libellé de la checkbox */
+    label: string;
+    /** Attribut name de la checkbox */
+    name?: string;
+    /** Taille de checkbox */
+    size?: ChecboxSize;
+    /** Texte additionnel de la checkbox */
+    hint?: string;
+    /** Désactive la checkbox */
+    disabled?: boolean;
+    /** Etat de la checkbox */
+    checked?: boolean;
+    /** Valeur de la checkbox */
+    value?: string;
+    /** Statut du message */
+    status?: "default" | "valid" | "error";
+    /** Texte du message d'erreur */
+    errorMessage?: string;
+    /** Texte du message de succès */
+    validMessage?: string;
+  }
+
+  const dispatch = createEventDispatcher();
+
+  let {
+    id,
+    label,
+    name,
+    size,
+    hint,
+    disabled,
+    checked = $bindable(),
+    value = $bindable(),
+    status = "default",
+    errorMessage,
+    validMessage,
+  }: Props = $props();
+
+  const sizeClass = $derived(`fr-checkbox-group--${size}`);
+
+  function handleChange(event: Event) {
+    const target = event.target as HTMLInputElement;
+    dispatch("valuechanged", target.value);
+  }
+</script>
+
+<div class={["fr-checkbox-group", sizeClass]}>
+  <input
+    type="checkbox"
+    {id}
+    {name}
+    {disabled}
+    {value}
+    bind:checked
+    aria-describedby={status ? `${id}-messages` : undefined}
+    onchange={handleChange}
+  />
+  <label class="fr-label" for={id}>
+    {label}
+
+    {#if hint}
+      <span class="fr-hint-text">{hint}</span>
+    {/if}
+  </label>
+
+  {#if status !== "default"}
+    <div class="fr-messages-group" id={status ? `${id}-messages` : undefined} aria-live="polite">
+      <p
+        class={["fr-message", `fr-message--${status}`]}
+        id={status ? `${id}-message-${status}` : undefined}
+      >
+        {validMessage || errorMessage}
+      </p>
+    </div>
+  {/if}
+</div>
+
+<style lang="scss">
+  @use "@gouvfr/dsfr/src/dsfr/core/main" as *;
+  @use "@gouvfr/dsfr/src/dsfr/component/form/main" as *;
+  @use "@gouvfr/dsfr/src/dsfr/component/checkbox/main" as *;
+</style>
