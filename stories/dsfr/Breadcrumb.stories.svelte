a8f5cae0a89fc205d9d93a902cfadea2181a2f58
Trésor ILOYI <tresor.iloyi@gmail.com>
2025-09-18 00:11:51 +0200

    [COMPOSANT] - DSFR - Ajoute le composant Breadcrumb

---
+++ stories/dsfr/Breadcrumb.stories.svelte
@@ -0,0 +1,36 @@
+<script module>
+  import { defineMeta } from "@storybook/addon-svelte-csf";
+  import {
+    breadcrumbArgTypes,
+    breadcrumbArgs,
+  } from "@gouvfr/dsfr/src/dsfr/component/breadcrumb/template/stories/breadcrumb-arg-types.js";
+  import DsfrBreadcrumb from "$lib/dsfr/DsfrBreadcrumb.svelte";
+
+  const { Story } = defineMeta({
+    title: "Composants/dsfr/Breadcrumb",
+    component: DsfrBreadcrumb,
+    argTypes: breadcrumbArgTypes,
+    args: {
+      ...breadcrumbArgs,
+      buttonAriaLabel: "vous êtes ici :",
+      buttonLabel: "Voir le fil d'Ariane",
+    },
+    parameters: {
+      backgrounds: {
+        options: {
+          blueFranceLab: { name: "Blue France Lab", value: "#000091" },
+        },
+      },
+    },
+  });
+</script>
+
+<Story name="Defaut" />
+
+<Story
+  name="Inverse"
+  args={{ inverse: true }}
+  globals={{
+    backgrounds: { value: "blueFranceLab" },
+  }}
+/>
