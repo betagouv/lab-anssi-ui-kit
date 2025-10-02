703d0f67f5373ab19f53b885fe7cb16130d89b44
Trésor ILOYI <tresor.iloyi@gmail.com>
2025-10-02 08:08:30 +0200

    [COMPOSANT] - DSFR - Ajoute le composant Logo

---
+++ src/lib/dsfr/DsfrLogo.svelte
@@ -0,0 +1,39 @@
+<svelte:options
+  customElement={{
+    tag: "dsfr-logo",
+    props: {
+      title: { attribute: "title", type: "String" },
+      size: { attribute: "size", type: "String" },
+    },
+  }}
+/>
+
+<script lang="ts">
+  import type { Size } from "$lib/types";
+
+  type LogoSize = Extract<Size, "sm" | "md" | "lg">;
+  interface Props {
+    /** Titre du logo (nom de l'entité) */
+    title: string;
+    /** Taille du logo */
+    size?: LogoSize;
+  }
+
+  let { title = "République<br>Française", size = "md" }: Props = $props();
+</script>
+
+<p class={["fr-logo", `fr-logo--${size}`]}>
+  {@html title}
+</p>
+
+<style lang="scss">
+  // DSFR Core styles
+  @import "@gouvfr/dsfr/src/dsfr/core/index";
+  @import "@gouvfr/dsfr/src/dsfr/core/style/typography/module/paragraph";
+  @import "@gouvfr/dsfr/src/dsfr/core/style/reset/module/box-sizing";
+  // DSFR Component styles
+  @import "@gouvfr/dsfr/dist/component/logo/logo.min.css";
+
+  @include set-shadow-host("inline-block");
+  @include set-dsfr-sizing("logo");
+</style>
