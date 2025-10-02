59f3dd416a18068f03f6d37da5df9f3f80a286fa
Trésor ILOYI <tresor.iloyi@gmail.com>
2025-10-02 08:08:30 +0200

    [COMPOSANT] - DSFR - Ajoute le composant Logo

---
+++ stories/dsfr/Logo.stories.svelte
@@ -0,0 +1,17 @@
+<script module>
+  import { defineMeta } from "@storybook/addon-svelte-csf";
+  import {
+    logoArgTypes,
+    logoArgs,
+  } from "@gouvfr/dsfr/src/dsfr/component/logo/template/stories/logo-arg-types.js";
+  import DsfrLogo from "$lib/dsfr/DsfrLogo.svelte";
+
+  const { Story } = defineMeta({
+    title: "Composants/dsfr/Logo",
+    component: DsfrLogo,
+    argTypes: logoArgTypes,
+    args: logoArgs,
+  });
+</script>
+
+<Story name="Défaut" />
