a16b64f0279e63c2a983a0aaff84a0714a7938be
Trésor ILOYI <tresor.iloyi@gmail.com>
2025-10-02 08:09:50 +0200

    [COMPOSANT] - DSFR - Ajoute le composant Footer

---
+++ stories/dsfr/Footer.stories.svelte
@@ -0,0 +1,52 @@
+<script module>
+  import { defineMeta } from "@storybook/addon-svelte-csf";
+  import {
+    footerArgTypes,
+    footerArgs,
+  } from "@gouvfr/dsfr/src/dsfr/component/footer/template/stories/footer-arg-types.js";
+
+  import DsfrFooter from "$lib/dsfr/DsfrFooter.svelte";
+
+  import PlaceholderPortrait from "@gouvfr/dsfr/example/img/placeholder.9x16.png";
+  import PlaceholderPaysage from "@gouvfr/dsfr/example/img/placeholder.16x9.png";
+
+  delete footerArgTypes.bottomCopyright;
+  delete footerArgTypes.hasTopFooter;
+  delete footerArgTypes.isMourning;
+  delete footerArgs.bottomCopyright;
+  delete footerArgs.hasTopFooter;
+  delete footerArgs.isMourning;
+
+  const { Story } = defineMeta({
+    title: "Composants/dsfr/Footer",
+    component: DsfrFooter,
+    argTypes: footerArgTypes,
+    args: {
+      ...footerArgs,
+      brandOperatorSrc: PlaceholderPortrait,
+      mainPartnerSrc: PlaceholderPaysage,
+      subPartners: [
+        {
+          src: PlaceholderPaysage,
+          href: "#",
+          alt: "Intitulé du partenaire secondaire",
+          style: "height: 5.625rem",
+        },
+        {
+          src: PlaceholderPaysage,
+          href: "#",
+          alt: "Intitulé du partenaire secondaire",
+          style: "height: 5.625rem",
+        },
+        {
+          src: PlaceholderPaysage,
+          href: "#",
+          alt: "Intitulé du partenaire secondaire",
+          style: "height: 5.625rem",
+        },
+      ],
+    },
+  });
+</script>
+
+<Story name="Défaut" />
