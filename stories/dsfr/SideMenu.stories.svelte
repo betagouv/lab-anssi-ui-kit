62e65d9a67eb38d590051d42a3a0ab18400e02dc
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2025-10-03 09:33:48 +0200

    Différencie les liens et les menus
    ...dans la story du menu latéral, afin d’éviter de les confondre

--- stories/dsfr/SideMenu.stories.svelte
+++ stories/dsfr/SideMenu.stories.svelte
@@ -1,16 +1,47 @@
-<script module>
-  import { defineMeta } from "@storybook/addon-svelte-csf";
+<script module lang="ts">
+  import DsfrSideMenu from "$lib/dsfr/DsfrSideMenu.svelte";
   import {
     sidemenuArgTypes,
     sidemenuArgs,
   } from "@gouvfr/dsfr/src/dsfr/component/sidemenu/template/stories/sidemenu-arg-types.js";
-  import DsfrSideMenu from "$lib/dsfr/DsfrSideMenu.svelte";
+  import { defineMeta } from "@storybook/addon-svelte-csf";
+
+  const getItemArgs = (id: string | number, type = "link", isActive = false) => ({
+    id: type === "link" ? `sidemenu-item-${id}` : `sidemenu-${id}`,
+    label: `Titre du ${type === "link" ? "lien" : "menu"} ${id}`,
+    href: type === "link" && "#",
+    type: type,
+    active: isActive,
+    isCollapsible: type === "menu",
+    collapseId: type === "menu" ? `sidemenu-${id}` : undefined,
+  });
 
   const { Story } = defineMeta({
     title: "Composants/dsfr/Side Menu",
     component: DsfrSideMenu,
     argTypes: sidemenuArgTypes,
-    args: { ...sidemenuArgs, buttonId: "sidemenu-collapse-1" },
+    args: {
+      ...sidemenuArgs,
+      items: [
+        {
+          ...getItemArgs(1, "menu", true),
+          items: [getItemArgs("1-1"), getItemArgs("1-2", "link", true), getItemArgs("1-3")],
+        },
+        getItemArgs(2),
+        {
+          ...getItemArgs(3, "menu"),
+          items: [
+            getItemArgs("3-1"),
+            {
+              ...getItemArgs("3-2", "menu"),
+              items: [getItemArgs("3-2-1"), getItemArgs("3-2-2"), getItemArgs("3-2-3")],
+            },
+            getItemArgs("3-3"),
+          ],
+        },
+      ],
+      buttonId: "sidemenu-collapse-1",
+    },
   });
 </script>
 
