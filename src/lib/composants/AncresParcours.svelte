90dff489315c1b4578070cec93675ea145badb58
Trésor ILOYI <tresor.iloyi@gmail.com>
2025-10-10 12:22:44 +0200

    [COMPOSANT] - LAB Ancres - Mise à jour des styles & du shadow

--- src/lib/composants/AncresParcours.svelte
+++ src/lib/composants/AncresParcours.svelte
@@ -20,6 +20,8 @@
   }
 
   let { ancres, indexActif = 0 }: Props = $props();
+  let scrollStart = $state(false);
+  let scrollEnd = $state(true);
 
   /**
    * Gère le clic sur une ancre de navigation.
@@ -40,10 +42,41 @@
       });
     }
   }
+
+  /**
+   * Gère l'affichage des ombres en fonction de la position du scroll.
+   * Met à jour les indicateurs `scrollStart` et `scrollEnd` selon la position du scroll :
+   * - Si le scroll est au début, `scrollStart` est false et `scrollEnd` est true.
+   * - Si le scroll est à la fin, `scrollStart` est true et `scrollEnd` est false.
+   * - Sinon, les deux sont true.
+   *
+   * @param {Event} event - L'événement de scroll déclenché sur le conteneur.
+   */
+  function handleScroll(event: Event) {
+    const container = event.currentTarget as HTMLElement;
+    const scrollLeft = container.scrollLeft;
+    const scrollWidth = container.scrollWidth;
+    const clientWidth = container.clientWidth;
+
+    if (scrollLeft === 0) {
+      scrollStart = false;
+      scrollEnd = true;
+    } else if (scrollLeft + clientWidth >= scrollWidth) {
+      scrollStart = true;
+      scrollEnd = false;
+    } else {
+      scrollStart = true;
+      scrollEnd = true;
+    }
+  }
 </script>
 
-<nav class="lab-anssi-ancres">
-  <div class="lab-anssi-ancres__conteneur">
+<nav
+  class="lab-anssi-ancres"
+  class:lab-anssi-ancres--shadow-start={scrollStart}
+  class:lab-anssi-ancres--shadow-end={scrollEnd}
+>
+  <div class="lab-anssi-ancres__conteneur" onscroll={handleScroll}>
     {#each ancres as ancre, index}
       <a
         href={ancre.cible}
@@ -57,7 +90,6 @@
 </nav>
 
 <style lang="scss">
-  @use "@gouvfr/dsfr/src/dsfr/core/style/color/module/_decisions.scss";
   @import "@gouvfr/dsfr/src/dsfr/core/style/selector/setting/breakpoint";
   @import "@gouvfr/dsfr/src/dsfr/core/style/selector/tool/breakpoint";
 
@@ -69,13 +101,36 @@
 
       &::after {
         content: "";
-        background: linear-gradient(90deg, rgba(30, 30, 30, 0) 0%, rgba(0, 0, 0, 0.16) 100%);
+        box-shadow:
+          inset 0 0 0 0 hsla(0, 0%, 9%, 0),
+          inset 0 0 0 0 hsla(0, 0%, 9%, 0);
         display: block;
         position: absolute;
         height: 100%;
-        width: 24px;
+        width: 100%;
         top: 0;
         right: 0;
+        pointer-events: none;
+      }
+
+      &--shadow-start::after {
+        box-shadow:
+          inset 2rem 0 1.5rem -2rem #161616,
+          inset 0 0 0 0 hsla(0, 0%, 9%, 0);
+      }
+
+      &--shadow-end {
+        &::after {
+          box-shadow:
+            inset 0 0 0 0 hsla(0, 0%, 9%, 0),
+            inset -2rem 0 1.5rem -2rem #161616;
+        }
+
+        &.lab-anssi-ancres--shadow-start::after {
+          box-shadow:
+            inset 2rem 0 1.5rem -2rem #161616,
+            inset -2rem 0 1.5rem -2rem #161616;
+        }
       }
     }
 
@@ -98,7 +153,7 @@
 
       // Reset link styles
       text-decoration: none;
-      color: var(--text-action-high-blue-france);
+      color: var(--text-action-high-grey);
 
       // Font properties
       font-weight: 500;
@@ -111,7 +166,7 @@
       }
 
       &:hover {
-        background-color: var(--background-alt-grey);
+        background-color: var(--background-default-grey-hover);
       }
 
       &:active {
@@ -125,6 +180,7 @@
       // Etat Actif
       &.active {
         background-color: var(--background-action-low-blue-france);
+        color: var(--text-action-high-blue-france);
 
         &:hover {
           background-color: var(--background-action-low-blue-france-hover);
