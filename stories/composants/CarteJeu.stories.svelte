e871c3b640545c2d6a41facc09ef721201fb2cee
Trésor ILOYI <tresor.iloyi@gmail.com>
2025-10-20 18:43:26 +0200

    [COMPOSANT] - LAB - Ajoute une story d'exemple de la CarteJeu

---
+++ stories/composants/CarteJeu.stories.svelte
@@ -0,0 +1,659 @@
+<script module>
+  import { defineMeta } from "@storybook/addon-svelte-csf";
+
+  import DsfrBadgesGroup from "$lib/dsfr/DsfrBadgesGroup.svelte";
+  import DsfrButton from "$lib/dsfr/DsfrButton.svelte";
+  import DsfrCard from "$lib/dsfr/DsfrCard.svelte";
+  import DsfrDropdown from "$lib/dsfr/DsfrDropdown.svelte";
+  import DsfrTagsGroup from "$lib/dsfr/DsfrTagsGroup.svelte";
+  import DsfrToggle from "$lib/dsfr/DsfrToggle.svelte";
+  import Reactions from "$lib/composants/Reactions.svelte";
+  import Icone from "$lib/lab/Icone.svelte";
+
+  import Placeholder from "@gouvfr/dsfr/example/img/placeholder.16x9.png";
+
+  const { Story } = defineMeta({
+    title: "Exemple/ANSSI/Carte Jeu",
+    args: {
+      reactions: [
+        { id: "1", emoji: "🔥", compteur: 0 },
+        { id: "2", emoji: "❤️", compteur: 0 },
+        { id: "3", emoji: "👍", compteur: 0 },
+      ],
+      style: "tertiaire-sans-bordure",
+      tooltipTexte: "Ajouter une réaction",
+      tooltipId: "tooltipReactions",
+      dropdownId: "dropdown-id",
+      dropdownCollapseId: "dropdown-collapse-id",
+      dropdownButtonTitle: "Choisir une option",
+      dropdownButtonKind: "tertiary-no-outline",
+      dropdownButtonSize: "sm",
+      dropdownButtonIcon: "more-line",
+      dropdownContentType: "custom",
+      dropdownAlign: "right",
+      dropdownButtonIconPlace: "only",
+      masque: false,
+    },
+    parameters: {
+      layout: "centered",
+    },
+  });
+</script>
+
+<Story name="Défaut">
+  {#snippet template(args)}
+    <div style="width: 300px;">
+      <!-- CarteJeu exemple -->
+      <div class="lab-anssi-carte-jeux">
+        <div class="lab-anssi-carte-jeux__header">
+          <div class="lab-anssi-carte-jeux__actions">
+            <DsfrButton
+              size="sm"
+              kind="tertiary-no-outline"
+              label="Modifier"
+              hasIcon
+              icon="edit-line"
+            />
+            <DsfrDropdown
+              align={args.dropdownAlign}
+              collapseId={args.dropdownCollapseId}
+              id={args.dropdownId}
+              buttonTitle={args.dropdownButtonTitle}
+              buttonKind={args.dropdownButtonKind}
+              buttonSize={args.dropdownButtonSize}
+              buttonIcon={args.dropdownButtonIcon}
+              buttonIconPlace={args.dropdownButtonIconPlace}
+            >
+              <DsfrToggle
+                left
+                label="Masquer de la vitrine des jeux"
+                id="toggleExemple"
+                onchange={() => (args.masque = !args.masque)}
+              />
+            </DsfrDropdown>
+          </div>
+
+          {#if args.masque}
+            <p class="lab-anssi-carte-jeux__masque">
+              <Icone nom="eye-off-line" />
+              Masqué de la vitrine des jeux
+            </p>
+          {/if}
+        </div>
+
+        <!-- Conteneur -->
+        <div class="lab-anssi-carte-jeux__conteneur">
+          <DsfrCard
+            title="Titre de la carte"
+            src={Placeholder}
+            hasHeaderBadge
+            hasDescription={false}
+            hasDetailStart={true}
+            hasDetailEnd={true}
+            detailStart="Détail"
+            disabled={false}
+            href="#"
+            size="sm"
+          >
+            <DsfrBadgesGroup
+              badges={[{ label: "Libellé badge", accent: "purple-glycine" }]}
+              size="sm"
+            />
+
+            <DsfrTagsGroup
+              tags={[{ id: "tag-1", label: "Libellé du tag" }]}
+              size="sm"
+              groupMarkup="div"
+            />
+          </DsfrCard>
+        </div>
+
+        <div class="lab-anssi-carte-jeux__reactions">
+          <Reactions
+            reactions={args.reactions}
+            style={args.style}
+            tooltipTexte={args.tooltipTexte}
+            tooltipId={args.tooltipId}
+          />
+        </div>
+      </div>
+    </div>
+
+    <style lang="scss">
+      .lab-anssi-carte-jeux {
+        &__header,
+        &__reactions {
+          border: 1px solid #dddddd;
+        }
+
+        &__header {
+          border-bottom-width: 0;
+        }
+
+        &__actions {
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          padding: 3px;
+        }
+
+        &__masque {
+          background-color: #e8edff;
+          display: flex;
+          align-items: center;
+          gap: 4px;
+          color: #0063cb;
+          font-size: 12px;
+          line-height: 20px;
+          padding: 4px 8px;
+          margin-block: 0;
+        }
+
+        &__reactions {
+          border-top-width: 0;
+          padding: 5px;
+        }
+      }
+    </style>
+  {/snippet}
+</Story>
+
+<Story
+  name="Cas Intermédiaire"
+  args={{
+    reactions: [
+      { id: "1", emoji: "🔥", compteur: 9 },
+      { id: "2", emoji: "❤️", compteur: 0 },
+      { id: "3", emoji: "👍", compteur: 0 },
+    ],
+  }}
+>
+  {#snippet template(args)}
+    <div style="width: 300px;">
+      <!-- CarteJeu exemple -->
+      <div class="lab-anssi-carte-jeux">
+        <div class="lab-anssi-carte-jeux__header">
+          <div class="lab-anssi-carte-jeux__actions">
+            <DsfrButton
+              size="sm"
+              kind="tertiary-no-outline"
+              label="Modifier"
+              hasIcon
+              icon="edit-line"
+            />
+            <DsfrDropdown
+              align={args.dropdownAlign}
+              collapseId={args.dropdownCollapseId}
+              id={args.dropdownId}
+              buttonTitle={args.dropdownButtonTitle}
+              buttonKind={args.dropdownButtonKind}
+              buttonSize={args.dropdownButtonSize}
+              buttonIcon={args.dropdownButtonIcon}
+              buttonIconPlace={args.dropdownButtonIconPlace}
+            >
+              <DsfrToggle
+                left
+                label="Masquer de la vitrine des jeux"
+                id="toggleExemple"
+                onchange={() => (args.masque = !args.masque)}
+              />
+            </DsfrDropdown>
+          </div>
+
+          {#if args.masque}
+            <p class="lab-anssi-carte-jeux__masque">
+              <Icone nom="eye-off-line" />
+              Masqué de la vitrine des jeux
+            </p>
+          {/if}
+        </div>
+
+        <!-- Conteneur -->
+        <div class="lab-anssi-carte-jeux__conteneur">
+          <DsfrCard
+            title="Titre de la carte"
+            src={Placeholder}
+            hasHeaderBadge
+            hasDescription={false}
+            hasDetailStart={true}
+            hasDetailEnd={true}
+            detailStart="Détail"
+            disabled={false}
+            href="#"
+            size="sm"
+          >
+            <DsfrBadgesGroup
+              badges={[{ label: "Libellé badge", accent: "purple-glycine" }]}
+              size="sm"
+            />
+
+            <DsfrTagsGroup
+              tags={[{ id: "tag-1", label: "Libellé du tag" }]}
+              size="sm"
+              groupMarkup="div"
+            />
+          </DsfrCard>
+        </div>
+
+        <div class="lab-anssi-carte-jeux__reactions">
+          <Reactions
+            reactions={args.reactions}
+            style={args.style}
+            tooltipTexte={args.tooltipTexte}
+            tooltipId={args.tooltipId}
+          />
+        </div>
+      </div>
+    </div>
+
+    <style lang="scss">
+      .lab-anssi-carte-jeux {
+        &__header,
+        &__reactions {
+          border: 1px solid #dddddd;
+        }
+
+        &__header {
+          border-bottom-width: 0;
+        }
+
+        &__actions {
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          padding: 3px;
+        }
+
+        &__masque {
+          background-color: #e8edff;
+          display: flex;
+          align-items: center;
+          gap: 4px;
+          color: #0063cb;
+          font-size: 12px;
+          line-height: 20px;
+          padding: 4px 8px;
+          margin-block: 0;
+        }
+
+        &__reactions {
+          border-top-width: 0;
+          padding: 5px;
+        }
+      }
+    </style>
+  {/snippet}
+</Story>
+
+<Story
+  name="Cas Intermédiaire (actif)"
+  args={{
+    reactions: [
+      { id: "1", emoji: "🔥", compteur: 9 },
+      { id: "2", emoji: "❤️", compteur: 12, actif: true },
+      { id: "3", emoji: "👍", compteur: 0 },
+    ],
+  }}
+>
+  {#snippet template(args)}
+    <div style="width: 300px;">
+      <!-- CarteJeu exemple -->
+      <div class="lab-anssi-carte-jeux">
+        <div class="lab-anssi-carte-jeux__header">
+          <div class="lab-anssi-carte-jeux__actions">
+            <DsfrButton
+              size="sm"
+              kind="tertiary-no-outline"
+              label="Modifier"
+              hasIcon
+              icon="edit-line"
+            />
+            <DsfrDropdown
+              align={args.dropdownAlign}
+              collapseId={args.dropdownCollapseId}
+              id={args.dropdownId}
+              buttonTitle={args.dropdownButtonTitle}
+              buttonKind={args.dropdownButtonKind}
+              buttonSize={args.dropdownButtonSize}
+              buttonIcon={args.dropdownButtonIcon}
+              buttonIconPlace={args.dropdownButtonIconPlace}
+            >
+              <DsfrToggle
+                left
+                label="Masquer de la vitrine des jeux"
+                id="toggleExemple"
+                onchange={() => (args.masque = !args.masque)}
+              />
+            </DsfrDropdown>
+          </div>
+
+          {#if args.masque}
+            <p class="lab-anssi-carte-jeux__masque">
+              <Icone nom="eye-off-line" />
+              Masqué de la vitrine des jeux
+            </p>
+          {/if}
+        </div>
+
+        <!-- Conteneur -->
+        <div class="lab-anssi-carte-jeux__conteneur">
+          <DsfrCard
+            title="Titre de la carte"
+            src={Placeholder}
+            hasHeaderBadge
+            hasDescription={false}
+            hasDetailStart={true}
+            hasDetailEnd={true}
+            detailStart="Détail"
+            disabled={false}
+            href="#"
+            size="sm"
+          >
+            <DsfrBadgesGroup
+              badges={[{ label: "Libellé badge", accent: "purple-glycine" }]}
+              size="sm"
+            />
+
+            <DsfrTagsGroup
+              tags={[{ id: "tag-1", label: "Libellé du tag" }]}
+              size="sm"
+              groupMarkup="div"
+            />
+          </DsfrCard>
+        </div>
+
+        <div class="lab-anssi-carte-jeux__reactions">
+          <Reactions
+            reactions={args.reactions}
+            style={args.style}
+            tooltipTexte={args.tooltipTexte}
+            tooltipId={args.tooltipId}
+          />
+        </div>
+      </div>
+    </div>
+
+    <style lang="scss">
+      .lab-anssi-carte-jeux {
+        &__header,
+        &__reactions {
+          border: 1px solid #dddddd;
+        }
+
+        &__header {
+          border-bottom-width: 0;
+        }
+
+        &__actions {
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          padding: 3px;
+        }
+
+        &__masque {
+          background-color: #e8edff;
+          display: flex;
+          align-items: center;
+          gap: 4px;
+          color: #0063cb;
+          font-size: 12px;
+          line-height: 20px;
+          padding: 4px 8px;
+          margin-block: 0;
+        }
+
+        &__reactions {
+          border-top-width: 0;
+          padding: 5px;
+        }
+      }
+    </style>
+  {/snippet}
+</Story>
+
+<Story
+  name="Cas Max"
+  args={{
+    reactions: [
+      { id: "1", emoji: "🔥", compteur: 9 },
+      { id: "2", emoji: "❤️", compteur: 12, actif: true },
+      { id: "3", emoji: "👍", compteur: 34 },
+    ],
+  }}
+>
+  {#snippet template(args)}
+    <div style="width: 300px;">
+      <!-- CarteJeu exemple -->
+      <div class="lab-anssi-carte-jeux">
+        <div class="lab-anssi-carte-jeux__header">
+          <div class="lab-anssi-carte-jeux__actions">
+            <DsfrButton
+              size="sm"
+              kind="tertiary-no-outline"
+              label="Modifier"
+              hasIcon
+              icon="edit-line"
+            />
+            <DsfrDropdown
+              align={args.dropdownAlign}
+              collapseId={args.dropdownCollapseId}
+              id={args.dropdownId}
+              buttonTitle={args.dropdownButtonTitle}
+              buttonKind={args.dropdownButtonKind}
+              buttonSize={args.dropdownButtonSize}
+              buttonIcon={args.dropdownButtonIcon}
+              buttonIconPlace={args.dropdownButtonIconPlace}
+            >
+              <DsfrToggle
+                left
+                label="Masquer de la vitrine des jeux"
+                id="toggleExemple"
+                onchange={() => (args.masque = !args.masque)}
+              />
+            </DsfrDropdown>
+          </div>
+
+          {#if args.masque}
+            <p class="lab-anssi-carte-jeux__masque">
+              <Icone nom="eye-off-line" />
+              Masqué de la vitrine des jeux
+            </p>
+          {/if}
+        </div>
+
+        <!-- Conteneur -->
+        <div class="lab-anssi-carte-jeux__conteneur">
+          <DsfrCard
+            title="Titre de la carte"
+            src={Placeholder}
+            hasHeaderBadge
+            hasDescription={false}
+            hasDetailStart={true}
+            hasDetailEnd={true}
+            detailStart="Détail"
+            disabled={false}
+            href="#"
+            size="sm"
+          >
+            <DsfrBadgesGroup
+              badges={[{ label: "Libellé badge", accent: "purple-glycine" }]}
+              size="sm"
+            />
+
+            <DsfrTagsGroup
+              tags={[{ id: "tag-1", label: "Libellé du tag" }]}
+              size="sm"
+              groupMarkup="div"
+            />
+          </DsfrCard>
+        </div>
+
+        <div class="lab-anssi-carte-jeux__reactions">
+          <Reactions
+            reactions={args.reactions}
+            style={args.style}
+            tooltipTexte={args.tooltipTexte}
+            tooltipId={args.tooltipId}
+          />
+        </div>
+      </div>
+    </div>
+
+    <style lang="scss">
+      .lab-anssi-carte-jeux {
+        &__header,
+        &__reactions {
+          border: 1px solid #dddddd;
+        }
+
+        &__header {
+          border-bottom-width: 0;
+        }
+
+        &__actions {
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          padding: 3px;
+        }
+
+        &__masque {
+          background-color: #e8edff;
+          display: flex;
+          align-items: center;
+          gap: 4px;
+          color: #0063cb;
+          font-size: 12px;
+          line-height: 20px;
+          padding: 4px 8px;
+          margin-block: 0;
+        }
+
+        &__reactions {
+          border-top-width: 0;
+          padding: 5px;
+        }
+      }
+    </style>
+  {/snippet}
+</Story>
+
+<Story name="Masqué" args={{ masque: true }}>
+  {#snippet template(args)}
+    <div style="width: 300px;">
+      <!-- CarteJeu exemple -->
+      <div class="lab-anssi-carte-jeux">
+        <div class="lab-anssi-carte-jeux__header">
+          <div class="lab-anssi-carte-jeux__actions">
+            <DsfrButton
+              size="sm"
+              kind="tertiary-no-outline"
+              label="Modifier"
+              hasIcon
+              icon="edit-line"
+            />
+            <DsfrDropdown
+              align={args.dropdownAlign}
+              collapseId={args.dropdownCollapseId}
+              id={args.dropdownId}
+              buttonTitle={args.dropdownButtonTitle}
+              buttonKind={args.dropdownButtonKind}
+              buttonSize={args.dropdownButtonSize}
+              buttonIcon={args.dropdownButtonIcon}
+              buttonIconPlace={args.dropdownButtonIconPlace}
+            >
+              <DsfrToggle
+                left
+                label="Masquer de la vitrine des jeux"
+                id="toggleExemple"
+                checked={args.masque}
+                onchange={() => (args.masque = !args.masque)}
+              />
+            </DsfrDropdown>
+          </div>
+
+          {#if args.masque}
+            <p class="lab-anssi-carte-jeux__masque">
+              <Icone nom="eye-off-line" />
+              Masqué de la vitrine des jeux
+            </p>
+          {/if}
+        </div>
+
+        <!-- Conteneur -->
+        <div class="lab-anssi-carte-jeux__conteneur">
+          <DsfrCard
+            title="Titre de la carte"
+            src={Placeholder}
+            hasHeaderBadge
+            hasDescription={false}
+            hasDetailStart={true}
+            hasDetailEnd={true}
+            detailStart="Détail"
+            disabled={false}
+            href="#"
+            size="sm"
+          >
+            <DsfrBadgesGroup
+              badges={[{ label: "Libellé badge", accent: "purple-glycine" }]}
+              size="sm"
+            />
+
+            <DsfrTagsGroup
+              tags={[{ id: "tag-1", label: "Libellé du tag" }]}
+              size="sm"
+              groupMarkup="div"
+            />
+          </DsfrCard>
+        </div>
+
+        <div class="lab-anssi-carte-jeux__reactions">
+          <Reactions
+            reactions={args.reactions}
+            style={args.style}
+            tooltipTexte={args.tooltipTexte}
+            tooltipId={args.tooltipId}
+          />
+        </div>
+      </div>
+    </div>
+
+    <style lang="scss">
+      .lab-anssi-carte-jeux {
+        &__header,
+        &__reactions {
+          border: 1px solid #dddddd;
+        }
+
+        &__header {
+          border-bottom-width: 0;
+        }
+
+        &__actions {
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          padding: 3px;
+        }
+
+        &__masque {
+          background-color: #e8edff;
+          display: flex;
+          align-items: center;
+          gap: 4px;
+          color: #0063cb;
+          font-size: 12px;
+          line-height: 20px;
+          padding: 4px 8px;
+          margin-block: 0;
+        }
+
+        &__reactions {
+          border-top-width: 0;
+          padding: 5px;
+        }
+      }
+    </style>
+  {/snippet}
+</Story>
