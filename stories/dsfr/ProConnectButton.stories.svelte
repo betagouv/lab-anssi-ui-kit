84074dbf2c8267f38221eb0bab3bcfcfef2fd3f8
Lionel Lalande <lionel.lalande@beta.gouv.fr>
2025-10-20 16:54:37 +0200

    [COMPOSANT] Ajoute le composant DSFR ProConnectButton

---
+++ stories/dsfr/ProConnectButton.stories.svelte
@@ -0,0 +1,11 @@
+<script module>
+  import { defineMeta } from "@storybook/addon-svelte-csf";
+  import DsfrProConnectButton from "$lib/dsfr/DsfrProConnectButton.svelte";
+
+  const { Story } = defineMeta({
+    title: "Composants/dsfr/ProConnectButton",
+    component: DsfrProConnectButton,
+  });
+</script>
+
+<Story name="Défaut" />
