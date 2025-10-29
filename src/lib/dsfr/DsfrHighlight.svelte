5d73a45f569064b82080841819c14452ed353f50
Trésor ILOYI <tresor.iloyi@gmail.com>
2025-10-29 23:09:13 +0100

    [COMPOSANT] - DSFR - Ajoute le composant Highlight

---
+++ src/lib/dsfr/DsfrHighlight.svelte
@@ -0,0 +1,52 @@
+<svelte:options
+  customElement={{
+    tag: "dsfr-highlight",
+    props: {
+      text: { attribute: "text", type: "String" },
+      id: { attribute: "id", type: "String" },
+      size: { attribute: "size", type: "String" },
+      accent: { attribute: "accent", type: "String" },
+    },
+  }}
+/>
+
+<script lang="ts">
+  import type { Accent, Size } from "$lib/types";
+
+  type HighlightSize = Extract<Size, "sm" | "md" | "lg">;
+  interface Props {
+    /** Contenu texte de la mise en exergue */
+    text: string;
+    /** Attribut id de la mise en exergue */
+    id?: string;
+    /** Taille du texte de la mise en exergue */
+    size?: HighlightSize;
+    /** Couleur d'accentuation */
+    accent?: Accent;
+  }
+
+  let { text, id, size = "md", accent }: Props = $props();
+
+  const accentClass = $derived(accent && `fr-highlight--${accent}`);
+</script>
+
+{#if text}
+  <div {id} class={["fr-highlight", accentClass]}>
+    <p class={`fr-text--${size}`}>{text}</p>
+  </div>
+{/if}
+
+<style lang="scss">
+  // DSFR Core styles
+  // @import "@gouvfr/dsfr/dist/core/core.min.css";
+  @import "@gouvfr/dsfr/src/dsfr/core/index";
+  @import "@gouvfr/dsfr/src/dsfr/core/style/typography/module/paragraph";
+  @import "@gouvfr/dsfr/src/dsfr/core/style/typography/module/font-weight";
+  @import "@gouvfr/dsfr/src/dsfr/core/style/typography/module/typography";
+  @import "@gouvfr/dsfr/src/dsfr/core/style/reset/module/box-sizing";
+  // DSFR Component styles
+  @import "@gouvfr/dsfr/dist/component/highlight/highlight.main.min.css";
+
+  @include set-shadow-host();
+  @include set-dsfr-sizing("highlight");
+</style>
---
+++ stories/dsfr/Highlight.stories.svelte
@@ -0,0 +1,17 @@
+<script module>
+  import { defineMeta } from "@storybook/addon-svelte-csf";
+  import {
+    highlightArgTypes,
+    highlightArgs,
+  } from "@gouvfr/dsfr/src/dsfr/component/highlight/template/stories/highlight-arg-types.js";
+  import DsfrHighlight from "$lib/dsfr/DsfrHighlight.svelte";
+
+  const { Story } = defineMeta({
+    title: "Composants/dsfr/Highlight",
+    component: DsfrHighlight,
+    argTypes: highlightArgTypes,
+    args: highlightArgs,
+  });
+</script>
+
+<Story name="Défaut" />
