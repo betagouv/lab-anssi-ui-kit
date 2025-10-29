1bcf99f9ee27246288c966e0359e32fb80567b9c
Trésor ILOYI <tresor.iloyi@gmail.com>
2025-10-29 23:09:13 +0100

    [COMPOSANT] - DSFR - Ajoute le composant Highlight

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -40,6 +40,7 @@ export { default as DsfrConnect } from "./dsfr/DsfrConnect.svelte";
 export { default as DsfrDropdown } from "./dsfr/DsfrDropdown.svelte";
 export { default as DsfrFooter } from "./dsfr/DsfrFooter.svelte";
 export { default as DsfrHeader } from "./dsfr/DsfrHeader.svelte";
+export { default as DsfrHighlight } from "./dsfr/DsfrHighlight.svelte";
 export { default as DsfrInput } from "./dsfr/DsfrInput.svelte";
 export { default as DsfrLink } from "./dsfr/DsfrLink.svelte";
 export { default as DsfrLogo } from "./dsfr/DsfrLogo.svelte";
