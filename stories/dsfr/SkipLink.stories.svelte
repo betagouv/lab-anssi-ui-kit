e3b29157d7a609d2f4820403bf2262ad77ff090c
Trésor ILOYI <tresor.iloyi@gmail.com>
2025-11-10 10:37:21 +0100

    [COMPOSANT] DSFR - Ajoute le composant SkipLink

---
+++ stories/dsfr/SkipLink.stories.svelte
@@ -0,0 +1,17 @@
+<script module>
+  import { defineMeta } from "@storybook/addon-svelte-csf";
+  import {
+    skiplinkArgTypes,
+    skiplinkArgs,
+  } from "@gouvfr/dsfr/src/dsfr/component/skiplink/template/stories/skiplink-arg-types.js";
+  import DsfrSkipLink from "$lib/dsfr/DsfrSkipLink.svelte";
+
+  const { Story } = defineMeta({
+    title: "Composants/dsfr/SkipLink",
+    component: DsfrSkipLink,
+    argTypes: skiplinkArgTypes,
+    args: skiplinkArgs,
+  });
+</script>
+
+<Story name="Défaut" />
