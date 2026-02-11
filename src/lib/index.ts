0a830e91b3d76110b11213ce609729fcf11d9a90
Trésor ILOYI <6053330+tiloyi@users.noreply.github.com>
2026-02-11 11:03:12 +0100

    [COMPOSANT] DSFR - Ajoute le composant Transcription

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -64,4 +64,5 @@ export { default as DsfrTextarea } from "./dsfr/DsfrTextarea.svelte";
 export { default as DsfrTile } from "./dsfr/DsfrTile.svelte";
 export { default as DsfrTagsGroup } from "./dsfr/DsfrTagsGroup.svelte";
 export { default as DsfrToggle } from "./dsfr/DsfrToggle.svelte";
+export { default as DsfrTranscription } from "./dsfr/DsfrTranscription.svelte";
 export { default as DsfrTranslate } from "./dsfr/DsfrTranslate.svelte";
