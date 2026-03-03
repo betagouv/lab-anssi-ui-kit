8cd1ae7b652a9195d24998696692fb933e398f37
Trésor ILOYI <6053330+tiloyi@users.noreply.github.com>
2026-03-03 14:54:03 +0100

    [COMPOSANT] - DsfrTable - Ajoute la structure de base du composant

---
+++ stories/dsfr/Table.stories.svelte
@@ -0,0 +1,55 @@
+<script module lang="ts">
+  import { defineMeta } from "@storybook/addon-svelte-csf";
+  import { type ComponentProps } from "svelte";
+
+  import {
+    tableArgTypes,
+    tableArgs,
+  } from "@gouvfr/dsfr/src/dsfr/component/table/template/stories/table-arg-types.js";
+
+  import DsfrTable from "$lib/dsfr/DsfrTable.svelte";
+  import tableData from "../utilitaires/tableData.json";
+
+  const { Story } = defineMeta({
+    title: "Composants/dsfr/Table",
+    component: DsfrTable,
+    argTypes: {
+      ...tableArgTypes,
+      layoutFixed: {
+        control: "boolean",
+        description: "Fixe la mise en page du tableau (table-layout: fixed)",
+      },
+      fixedFirstCellHead: {
+        control: "boolean",
+        description: "Fixe la première cellule de l'en-tête (sticky)",
+      },
+    },
+    args: {
+      ...tableArgs,
+      layoutFixed: false,
+      fixedFirstCellHead: false,
+    },
+    render: template,
+  });
+
+  type Args = ComponentProps<DsfrTable>;
+</script>
+
+{#snippet template(args: Args)}
+  <dsfr-table
+    id={args.id}
+    caption={args.caption}
+    caption-detail={args.captionDetail}
+    no-caption={args.noCaption || undefined}
+    caption-bottom={args.captionBottom || undefined}
+    bordered={args.bordered || undefined}
+    no-scroll={args.noScroll || undefined}
+    multiline={args.multiline || undefined}
+    layout-fixed={args.layoutFixed || undefined}
+    size={args.size}
+    fixed-first-cell-head={args.fixedFirstCellHead || undefined}
+    table={tableData}
+  ></dsfr-table>
+{/snippet}
+
+<Story name="Defaut" />
