40224551f0154977aefacb910d516fa714796b43
Lionel Lalande <lionel.lalande@beta.gouv.fr>
2026-03-05 18:14:10 +0100

    [NONCE] Corrige le problème de CSP
    ... suite à la montée de version de Svelte

--- outils/injection-nonce.ts
+++ outils/injection-nonce.ts
@@ -1,12 +1,19 @@
 export const injecteNonceWebcomponents = (code: string) => {
   let codeAvecNonce = `const nonce = document.currentScript?.nonce;\n${code}`;
+
+  const createElementFuncName =
+    code.match(/function (.+)\(.,.,.\){return document.createElementNS\(/g)?.[1] ??
+    "document.createElement";
+
+  const modeleRecherche = `const (.)\\s*=\\s*${createElementFuncName}\\(["']style["']\\);`;
+
   codeAvecNonce = codeAvecNonce
     .replace(
       /const (.)\s*=\s*.\(["']style["']\);/gm,
       (match, nomVariable) => `${match}${nomVariable}.nonce=nonce;`,
     )
     .replace(
-      /const (.)\s*=\s*document\.createElement\(["']style["']\);/gm,
+      new RegExp(modeleRecherche, "gm"),
       (match, nomVariable) => `${match}${nomVariable}.nonce=nonce;`,
     );
 
