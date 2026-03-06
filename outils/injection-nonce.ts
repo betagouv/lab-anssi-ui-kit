e5089a3fd07b4078afcbb31fcb9e9921379d7fa8
Trésor ILOYI <6053330+tiloyi@users.noreply.github.com>
2026-03-06 01:00:42 +0100

    [NONCE] Corrige les expressions régulières permettant l'injection du nonce dans les styles

--- outils/injection-nonce.ts
+++ outils/injection-nonce.ts
@@ -1,12 +1,13 @@
 export const injecteNonceWebcomponents = (code: string) => {
   let codeAvecNonce = `const nonce = document.currentScript?.nonce;\n${code}`;
+
   codeAvecNonce = codeAvecNonce
     .replace(
-      /const (.)\s*=\s*.\(["']style["']\);/gm,
+      /const (\w+)\s*=\s*\w+\(["']style["']\);/gm,
       (match, nomVariable) => `${match}${nomVariable}.nonce=nonce;`,
     )
     .replace(
-      /const (.)\s*=\s*document\.createElement\(["']style["']\);/gm,
+      /const (\w+)\s*=\s*document\.createElement\(["']style["']\);/gm,
       (match, nomVariable) => `${match}${nomVariable}.nonce=nonce;`,
     );
 
